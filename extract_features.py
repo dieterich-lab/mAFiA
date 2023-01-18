9965fba7428728c68fa067f0dcd1721eda10281b
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-01-18 12:00:22 +0100

    implemented feature extraction

--- extract_features.py
+++ extract_features.py
@@ -1,15 +1,101 @@
-import os
+import os, sys
 HOME = os.path.expanduser('~')
 from utils import segment
 import torch
-from models import objectview
+import numpy as np
+from models import objectview, rodan
+from fast_ctc_decode import beam_search
 
 model_path = os.path.join(HOME, 'pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch')
 torchdict = torch.load(model_path, map_location="cpu")
 origconfig = torchdict["config"]
 config = objectview(origconfig)
-config.batchsize = 16
+
+def convert_statedict(state_dict):
+    from collections import OrderedDict
+    new_checkpoint = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:] # remove module.
+        new_checkpoint[name] = v
+    return new_checkpoint
+
+activation = {}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
+
+def load_model(modelfile, config):
+    if modelfile == None:
+        sys.stderr.write("No model file specified!")
+        sys.exit(1)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+    model = rodan(config=config).to(device)
+    state_dict = torch.load(modelfile, map_location=device)["state_dict"]
+    if "state_dict" in state_dict:
+        model.load_state_dict(convert_statedict(state_dict["state_dict"]))
+    else:
+        model.load_state_dict(torch.load(modelfile, map_location=device)["state_dict"])
+    model.convlayers.conv21.register_forward_hook(get_activation('conv21'))
+
+    print(model)
+    model.eval()
+    torch.set_grad_enabled(False)
+
+    return model, device
+
+vocab = { 1:"A", 2:"C", 3:"G", 4:"T" }
+alphabet = "".join(["N"] + list(vocab.values()))
+
+def ctcdecoder(logits, label, blank=False, beam_size=5, alphabet=alphabet, pre=None):
+    ret = np.zeros((label.shape[0], label.shape[1]+50))
+    retstr = []
+    for i in range(logits.shape[0]):
+        if pre is not None:
+            beamcur = beam_search(torch.softmax(torch.tensor(pre[:,i,:]), dim=-1).cpu().detach().numpy(), alphabet=alphabet, beam_size=beam_size)[0]
+        prev = None
+        cur = []
+        pos = 0
+        for j in range(logits.shape[1]):
+            if not blank:
+                if logits[i,j] != prev:
+                    prev = logits[i,j]
+                    try:
+                        if prev != 0:
+                            ret[i, pos] = prev
+                            pos+=1
+                            cur.append(vocab[prev])
+                    except:
+                        sys.stderr.write("ctcdecoder: fail on i:", i, "pos:", pos)
+            else:
+                if logits[i,j] == 0: break
+                ret[i, pos] = logits[i,j] # is this right?
+                cur.append(vocab[logits[i,pos]])
+                pos+=1
+        if pre is not None:
+            retstr.append(beamcur)
+        else:
+            retstr.append("".join(cur))
+    return ret, retstr
 
 def extract_features_from_signal(signal, pos):
     chunks = segment(signal, config.seqlen)
-    return
+    model, device = load_model(model_path, config)
+    event = torch.unsqueeze(torch.FloatTensor(chunks), 1).to(device, non_blocking=True)
+    out = model.forward(event)
+
+    pred_labels = []
+    features = []
+    for i in range(out.shape[1]):
+        this_pred_label, pred_locs = beam_search(torch.softmax(out[:, i, :], dim=-1).cpu().detach().numpy(), alphabet=alphabet)
+        pred_labels.append(this_pred_label)
+        this_feature = activation['conv21'].detach().cpu().numpy()[i, :, pred_locs]
+        features.append(this_feature)
+
+    pred_label = ''.join(pred_labels)
+    all_features = np.vstack(features)
+
+    ### todo: check pred label ###
+
+    return all_features[pos]
