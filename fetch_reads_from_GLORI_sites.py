4144b1e0c796bbcc57ff03351595ed6d7bd259ae
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-01-19 15:33:07 +0100

    clean up

--- fetch_reads_from_GLORI_sites.py
+++ fetch_reads_from_GLORI_sites.py
@@ -21,96 +21,68 @@ for record in SeqIO.parse(ref_file, 'fasta'):
     if (record.id.isnumeric()) or (record.id in ['X', 'Y']):
         ref[record.id] = str(record.seq)
 
-# bam_file = os.path.join(HOME, 'Data/Isabel_IVT_Nanopore/HEK293A_wildtype/minimap/aligned_genome.bam.sorted')
 bam_file = os.path.join(HOME, 'inference/HEK293_IVT_2_q50_10M/epoch29_HEK293A_WT_aligned_to_genome.bam.sorted')
 bam = pysam.AlignmentFile(bam_file, 'rb')
 
 ### index read ids ###
-# fast5_dir = os.path.join(HOME, 'Data/Isabel_IVT_Nanopore/HEK293A_wildtype/fast5_filtered')
 fast5_dir = '/prj/Isabel_IVT_Nanopore/HEK293A_wildtype/Jessica_HEK293/HEK293A_2/20190409_1503_GA10000_FAK10978_2e75d7be/fast5_all'
 # id_signal = {}
 index_read_ids = {}
 print('Indexing read ids...')
-for f5_filepath in tqdm(glob(os.path.join(fast5_dir, '*.fast5'), recursive=True)):
+for f5_filepath in glob(os.path.join(fast5_dir, '*.fast5'), recursive=True):
     f5 = get_fast5_file(f5_filepath, mode="r")
     for read_id in f5.get_read_ids():
         index_read_ids[read_id] = f5_filepath
 
 ### search by GLORI sites ###
 print('Going through GLORI m6A sites...')
-for ind, row in tqdm(df_glori.iterrows()):
+for ind, row in df_glori.iterrows():
     # ### debug ###
-    ind = 4682
-    row = df_glori.iloc[ind]
+    # ind = 4682
+    # row = df_glori.iloc[ind]
 
     chr = row['Chr'].lstrip('chr')
     strand = row['Strand']
     site = row['Sites'] - 1   # 0-based
-    ratio = row['Ratio']
+    glori_ratio = row['Ratio']
 
     if not ((chr.isnumeric()) or (chr in ['X', 'Y']) or (strand=='+')):
         continue
 
     ref_motif = ref[chr][site-2:site+3]
-    # if strand=='-':
-    #     ref_motif = str(Seq(ref_motif).reverse_complement())
-
-    # overlap_reads = bam.fetch(chr, site, site+1)
-    # for this_read in overlap_reads:
-    #     print(str(this_read))
 
     site_features = {}
     for pileupcolumn in bam.pileup(chr, site, site+1, truncate=True, min_base_quality=0):
         if pileupcolumn.pos == site:
-            # coverage = pileupcolumn.n
             coverage = pileupcolumn.get_num_aligned()
             if coverage>100:
                 valid_counts = 0
-                print('\nCollecting features for site {}, chr{}, pos{}, strand{} ...'.format(ind, chr, pileupcolumn.pos, strand))
+                print('\nCollecting features for site {}, chr{}, pos{}, strand{}'.format(ind, chr, pileupcolumn.pos, strand))
                 for pileupread in tqdm(pileupcolumn.pileups):
                     query_name = pileupread.alignment.query_name
-                    # query_position = pileupread.query_position_or_next
-                    query_position = pileupread.query_position
+                    query_position = pileupread.query_position_or_next
+                    # query_position = pileupread.query_position
                     flag = pileupread.alignment.flag
-                    # print(query_name, query_position, flag, pileupread.alignment.query_sequence[query_position-2:query_position+3])
-                    # if (flag!=1):
-                    #     continue
-                    # if flag==16:
-                    #     reverse_complement = True
-                    # else:
-                    #     reverse_complement = False
-                    if query_position and (flag==0) and (query_name in index_read_ids.keys()) and (pileupread.alignment.query_sequence[query_position] == 'A'):
-                    # if query_position and (query_name in index_read_ids.keys()) and (pileupread.alignment.query_sequence[query_position] == 'A'):
+
+                    # if query_position and (flag==0) and (query_name in index_read_ids.keys()) and (pileupread.alignment.query_sequence[query_position] == 'A'):
+                    if query_position and (flag==0) and (query_name in index_read_ids.keys()):
                         valid_counts += 1
-                        # print('\tmotif in read {} = {}, pos {}'.format(
-                        #     query_name,
-                        #     pileupread.alignment.query_sequence[query_position-2:query_position+3],
-                        #     query_position
-                        # ))
                         query_motif = pileupread.alignment.query_sequence[query_position-2:query_position+3]
                         this_read_signal = get_norm_signal_from_read_id(query_name, index_read_ids)
                         this_read_features = extract_features_from_signal(this_read_signal, query_position, query_motif)
-                        # print('{}, pos {}'.format(query_name, query_position))
-                        # print(this_read_features)
                         if this_read_features is not None:
                             site_features[query_name] = this_read_features
-                        # else:
-                        #     print('Query read not in fast5 directory!')
 
                 if valid_counts>0:
                     print('Reference motif = {}'.format(ref_motif))
-                    print('Mod. ratio = {}'.format(ratio))
+                    print('Mod. ratio = {}'.format(glori_ratio))
                     print('coverage = {}'.format(coverage))
                     print('Valid reads = {}'.format(valid_counts))
-                    print('{} feature vectors collected\n'.format(len(site_features)))
+                    print('{} feature vectors collected'.format(len(site_features)))
                 else:
-                    print('No valid reads!\n')
+                    print('No valid reads!')
 
-    outlier_ratio = cluster_features(site_features)
-
-    # if strand=='+':
-    #     # print(ref_seq)
-    #     motifs.append(ref_seq)
-    # else:
-    #     # print(Seq(ref_seq).reverse_complement())
-    #     motifs.append(str(Seq(ref_seq).reverse_complement()))
+    if len(site_features)>0:
+        print('Now clustering features...')
+        outlier_ratio = cluster_features(site_features)
+        print('Calculated outlier ratio {:.3f} [GLORI {:.3f}]'.format(outlier_ratio, glori_ratio))
