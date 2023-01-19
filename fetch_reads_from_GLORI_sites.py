25d745681f1116c6ddf516f06f4ba5cee97247f3
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-01-19 16:52:53 +0100

    last backup of the day

--- fetch_reads_from_GLORI_sites.py
+++ fetch_reads_from_GLORI_sites.py
@@ -1,6 +1,7 @@
 import os
 HOME = os.path.expanduser('~')
 from glob import glob
+from utils import med_mad
 from tqdm import tqdm
 import pandas as pd
 import pysam
@@ -24,15 +25,27 @@ for record in SeqIO.parse(ref_file, 'fasta'):
 bam_file = os.path.join(HOME, 'inference/HEK293_IVT_2_q50_10M/epoch29_HEK293A_WT_aligned_to_genome.bam.sorted')
 bam = pysam.AlignmentFile(bam_file, 'rb')
 
+### collect read ids ###
+query_read_ids = []
+for read in bam.fetch():
+    if read.flag==0:
+        query_read_ids.append(read.query_name)
+
 ### index read ids ###
 fast5_dir = '/prj/Isabel_IVT_Nanopore/HEK293A_wildtype/Jessica_HEK293/HEK293A_2/20190409_1503_GA10000_FAK10978_2e75d7be/fast5_all'
-# id_signal = {}
-index_read_ids = {}
-print('Indexing read ids...')
-for f5_filepath in glob(os.path.join(fast5_dir, '*.fast5'), recursive=True):
+id_signal = {}
+# index_read_ids = {}
+print('Collecting fast5 reads...')
+### todo: parallelize ###
+for f5_filepath in tqdm(glob(os.path.join(fast5_dir, '*.fast5'), recursive=True)):
     f5 = get_fast5_file(f5_filepath, mode="r")
     for read_id in f5.get_read_ids():
-        index_read_ids[read_id] = f5_filepath
+        # index_read_ids[read_id] = f5_filepath
+        if read_id in query_read_ids:
+            read = f5.get_read(read_id)
+            signal = read.get_raw_data(scale=True)
+            med, mad = med_mad(signal)
+            id_signal[read_id] = (signal - med) / mad
 
 ### search by GLORI sites ###
 print('Going through GLORI m6A sites...')
@@ -61,10 +74,11 @@ for ind, row in df_glori.iterrows():
                     flag = pileupread.alignment.flag
 
                     # if query_position and (flag==0) and (query_name in index_read_ids.keys()) and (pileupread.alignment.query_sequence[query_position] == 'A'):
-                    if query_position and (flag==0) and (query_name in index_read_ids.keys()):
+                    if query_position and (flag==0) and (query_name in id_signal.keys()):
                         valid_counts += 1
                         query_motif = pileupread.alignment.query_sequence[query_position-2:query_position+3]
-                        this_read_signal = get_norm_signal_from_read_id(query_name, index_read_ids)
+                        # this_read_signal = get_norm_signal_from_read_id(query_name, index_read_ids)
+                        this_read_signal = id_signal[query_name]
                         this_read_features = extract_features_from_signal(this_read_signal, query_position, query_motif)
                         if this_read_features is not None:
                             site_features[query_name] = this_read_features
