c46c00016e26c606b9ede0d06b4fb23cd6485cd0
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-02-08 15:51:46 +0100

    small formatting updates

--- cluster_features_rRNA.py
+++ cluster_features_rRNA.py
@@ -18,6 +18,7 @@ from time import time
 import random
 random.seed(10)
 from random import sample
+import numpy as np
 
 # parser = argparse.ArgumentParser()
 # parser.add_argument('--perc_thresh', help='percolation threshold')
@@ -26,9 +27,11 @@ from random import sample
 # print('Clustering at threshold {:.2f}'.format(PERC_THRESH))
 # PERC_THRESH = 0.9
 
+mod_type = 'psU'
+rRNA_species = 'NR_003286_RNA18SN5'
 mod_file = os.path.join(HOME, 'Data/rRNA/only_mod.bed')
 df_mod = pd.read_csv(mod_file, names=['sample', 'start', 'stop', 'mod'], sep='\t')
-df_mod_Am = df_mod[(df_mod['mod']=='Am') & (df_mod['sample']=='NR_003286_RNA18SN5')]
+df_mod_sel = df_mod[(df_mod['mod'] == mod_type) & (df_mod['sample'] == rRNA_species)]
 
 ref_file = os.path.join(HOME, 'Data/transcriptomes/rRNA_18S_28S.fasta')
 ref = {}
@@ -87,10 +90,11 @@ ivt_predStr_features = get_features_from_collection_of_signals(fixed_model, fixe
 
 ### search by GLORI sites ###
 MIN_COVERAGE = 50
-outfile = os.path.join(HOME, 'inference/rRNA/MAFIA_outlier_ratios.tsv')
+sigma_thresh = 0.5
+outfile = os.path.join(HOME, 'inference/rRNA/{}_outlier_ratios_{}_sigma{:.2f}.tsv'.format(rRNA_species, mod_type, sigma_thresh))
 df_outlier = pd.DataFrame()
 counts = 0
-for ind, row in df_mod_Am.iterrows():
+for ind, row in df_mod_sel.iterrows():
     # ind = 0
     # row = df_mod_Am.iloc[ind]
     print('\nSite {}'.format(ind), flush=True)
@@ -112,14 +116,14 @@ for ind, row in df_mod_Am.iterrows():
         print('{} feature vectors collected from IVT'.format(len(ivt_site_motif_features)), flush=True)
         print('Now clustering features...', flush=True)
         # outlier_ratio = get_outlier_ratio_from_features(ivt_site_motif_features, wt_site_motif_features, ref_motif, PERC_THRESH)
-        outlier_ratio = get_outlier_ratio_from_features_v3(ivt_site_motif_features, wt_site_motif_features, ref_motif, 1.0)
+        outlier_ratio = get_outlier_ratio_from_features_v3(ivt_site_motif_features, wt_site_motif_features, ref_motif, sigma_thresh)
         print('Calculated outlier ratio {:.2f}'.format(outlier_ratio), flush=True)
         # print('Calculated outlier ratio {:.2f} [GLORI {:.2f}]'.format(outlier_ratio, glori_ratio), flush=True)
         print('=========================================================', flush=True)
         if outlier_ratio!=-1:
             new_row = row.copy()
             new_row['motif'] = ref_motif
-            new_row['ratio_outlier'] = outlier_ratio
+            new_row['ratio_outlier'] = np.round(outlier_ratio, 2)
             new_row['num_features_ivt'] = len(ivt_site_motif_features)
             new_row['num_features_wt'] = len(wt_site_motif_features)
             df_outlier = pd.concat([df_outlier, new_row.to_frame().T])
