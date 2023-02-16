403325cbcd38f415de1bd2d50a734e7677b0b86f
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-02-16 16:55:14 +0100

    last update of the day

--- plot_svm_mod_ratios.py
+++ plot_svm_mod_ratios.py
@@ -4,34 +4,35 @@ import matplotlib
 matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 
-# tsv_dir = '/home/adrian/inference/rRNA/WBSCR22_mixing'
-tsv_dir = '/home/adrian/inference/rRNA/METTL5_mixing'
 img_out = '/home/adrian/img_out'
 
-# mod = 'm7G'
-# stop = 1639
-# mixes = [
-#     '0WT_100MUT',
-#     '25WT_75MUT',
-#     '50WT_50MUT',
-#     '75WT_25MUT',
-#     '100WT_0MUT',
-# ]
-
-mod = 'm6A'
-stop = 1832
+tsv_dir = '/home/adrian/inference/rRNA/WBSCR22_mixing'
+mod = 'm7G'
+stop = 1639
 mixes = [
-    '0WT_100KO',
-    '25WT_75KO',
-    '50WT_50KO',
-    '75WT_25KO',
-    '100WT_0KO',
+    '0WT_100MUT',
+    '25WT_75MUT',
+    '50WT_50MUT',
+    '75WT_25MUT',
+    '100WT_0MUT',
 ]
+
+# tsv_dir = '/home/adrian/inference/rRNA/METTL5_mixing'
+# mod = 'm6A'
+# stop = 1832
+# mixes = [
+#     '0WT_100KO',
+#     '25WT_75KO',
+#     '50WT_50KO',
+#     '75WT_25KO',
+#     '100WT_0KO',
+# ]
 
 mix_mod_ratio = {}
 mix_num_features = {}
 for this_mix in mixes:
     df = pd.read_csv(os.path.join(tsv_dir, 'svm_mod_ratio_{}.tsv'.format(this_mix)), sep='\t')
+    # df = pd.read_csv(os.path.join(tsv_dir, 'svm_opt_thresh_mod_ratio_{}.tsv'.format(this_mix)), sep='\t')
     df_mod = df[(df['stop'] == stop) & (df['mod'] == mod)]
     mod_ratio = df_mod['mod_ratio'].values[0]
     num_test_features = df_mod['num_test_features'].values[0]
