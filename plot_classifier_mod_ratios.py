fb3a099ede64dba0c1ee1458159b8c9e2fc3e1b6
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-02-21 08:18:47 +0100

    renamed script

--- plot_classifier_mod_ratios.py
+++ plot_classifier_mod_ratios.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 
 img_out = '/home/adrian/img_out'
 
-tsv_dir = '/home/adrian/inference/rRNA/WBSCR22_mixing'
-mod = 'm7G'
-stop = 1639
+tsv_dir = '/home/adrian/inference/rRNA/METTL5_mixing_viterbi/'
+mod = 'm6A'
+stop = 1832
 mixes = [
-    '0WT_100MUT',
-    '25WT_75MUT',
-    '50WT_50MUT',
-    '75WT_25MUT',
-    '100WT_0MUT',
+    'IVT',
+    '0WT_100KO',
+    '50WT_50KO',
+    '100WT_0KO',
+    'WT'
 ]
 
 # tsv_dir = '/home/adrian/inference/rRNA/METTL5_mixing'
@@ -28,10 +28,12 @@ mixes = [
 #     '100WT_0KO',
 # ]
 
+classifier = 'log_reg'
+
 mix_mod_ratio = {}
 mix_num_features = {}
 for this_mix in mixes:
-    df = pd.read_csv(os.path.join(tsv_dir, 'svm_mod_ratio_{}.tsv'.format(this_mix)), sep='\t')
+    df = pd.read_csv(os.path.join(tsv_dir, '{}_mod_ratio_{}.tsv'.format(classifier, this_mix)), sep='\t')
     # df = pd.read_csv(os.path.join(tsv_dir, 'svm_opt_thresh_mod_ratio_{}.tsv'.format(this_mix)), sep='\t')
     df_mod = df[(df['stop'] == stop) & (df['mod'] == mod)]
     mod_ratio = df_mod['mod_ratio'].values[0]
@@ -48,5 +50,5 @@ plt.xlabel('Mix', fontsize=15)
 plt.ylabel('Pred. mod. ratio', fontsize=15)
 plt.title('{} {}'.format(mod, stop), fontsize=20)
 
-plt.savefig(os.path.join(img_out, 'svm_mod_ratios_{}_{}.png'.format(mod, stop)), bbox_inches='tight')
+plt.savefig(os.path.join(img_out, '{}_mod_ratios_{}_{}.png'.format(classifier, mod, stop)), bbox_inches='tight')
 plt.close('all')
