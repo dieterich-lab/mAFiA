1cffbe59e8863ff8d28fb2f792347ec2f49bd681
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-02-28 16:56:35 +0100

    small updates to plotting

--- plot_correlation_glori_mod_ratio.py
+++ plot_correlation_glori_mod_ratio.py
@@ -9,7 +9,7 @@ img_out = os.path.join(HOME, 'img_out', 'MAFIA_MaxAbs_multiple')
 if not os.path.exists(img_out):
     os.makedirs(img_out, exist_ok=True)
 
-P_VAL_THRESH = 1.0E-99
+P_VAL_THRESH = 1.0E-10
 NUM_READS_THRESH = 20
 df_all = pd.read_csv(df_file, sep='\t')
 df = df_all[(df_all['P_adjust']<=P_VAL_THRESH) * (df_all['num_test_features']>=NUM_READS_THRESH)]
@@ -28,8 +28,8 @@ for this_motif in motifs:
     plt.ylim([-0.05, 1.05])
     plt.xlabel('GLORI (p_val_thresh {:.2E})'.format(P_VAL_THRESH), fontsize=15)
     plt.ylabel('Predicted mod. ratio', fontsize=15)
-    plt.title('{}, {} sites\nCorrelation {:.2f}'.format(this_motif, df_motif.shape[0], corr), fontsize=20)
+    plt.title('{}, {} sites >= {} reads\nCorrelation {:.2f}'.format(this_motif, df_motif.shape[0], NUM_READS_THRESH, corr), fontsize=20)
     plt.show()
 
-    plt.savefig(os.path.join(img_out, 'corr_glori_mod_ratio_{}_p_val_thresh_{:.2E}.png'.format(this_motif, P_VAL_THRESH)), bbox_inches='tight')
+    plt.savefig(os.path.join(img_out, 'corr_glori_modRatio_{}_minNumReads{}_pValThresh{:.2E}.png'.format(this_motif, NUM_READS_THRESH, P_VAL_THRESH)), bbox_inches='tight')
     plt.close()
