70d4a9894cdb7aa0c3cb46ff27aeeb966e75708a
Adrian <adrian.kaho.chan@gmail.com>
2023-03-14 09:23:40 +0100

    added array basecaller script

---
+++ array_basecaller.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --verbose
+#SBATCH --job-name=basecaller
+#SBATCH --output=/home/achan/slurm/basecaller_%02a.out
+
+set -e -u
+
+source ${HOME}/git/renata/virtualenv/bin/activate
+
+printf -v PART '%02d' "${SLURM_ARRAY_TASK_ID}"
+LIST_FILENAMES=${WORKSPACE}/${FILENAME_PREFIX}${PART}
+OUTPUT=${FASTA}${PART}
+echo "Running basecaller on ${LIST_FILENAMES}"
+
+python3 ${HOME}/git/renata/basecall_viterbi.py \
+--fast5dir ${FAST5_DIR} \
+--list_filenames ${LIST_FILENAMES} \
+--arch ${ARCH} \
+--model ${MODEL} \
+--decoder viterbi \
+> ${OUTPUT}
