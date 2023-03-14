dc01271796d6ffd7199f7ecce5d563e15f046dd1
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-03-14 13:59:58 +0100

    revert to training on passed reads only

--- MAFIA_train_binary_classifier_oligo.sh
+++ MAFIA_train_binary_classifier_oligo.sh
@@ -17,9 +17,9 @@ WORKSPACE=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Adrian
 
 python3 train_binary_classifier.py \
 --unm_bam_file ${WORKSPACE}/mapping/A_RTA_sorted_filtered.bam \
---unm_fast5_dir /prj/TRR319_RMaP/Project_BaseCalling/Isabel/20230221_WUE_splint_lig/WUE_splint_lig_A_RTA/20230221_1328_X1_ANS648_701f60ca \
+--unm_fast5_dir /prj/TRR319_RMaP/Project_BaseCalling/Isabel/20230221_WUE_splint_lig/WUE_splint_lig_A_RTA/20230221_1328_X1_ANS648_701f60ca/fast5_pass \
 --mod_bam_file ${WORKSPACE}/mapping/m6A_RTA_sorted_filtered.bam \
---mod_fast5_dir /prj/TRR319_RMaP/Project_BaseCalling/Isabel/20230221_WUE_splint_lig/WUE_splint_lig_m6A_RTA/20230221_1328_X2_ANS491_f891b4b9 \
+--mod_fast5_dir /prj/TRR319_RMaP/Project_BaseCalling/Isabel/20230221_WUE_splint_lig/WUE_splint_lig_m6A_RTA/20230221_1328_X2_ANS491_f891b4b9/fast5_pass \
 --ref_file ${WORKSPACE}/reference/splint_variations_max_blocks_7.fasta \
 --max_num_reads -1 \
 --min_coverage 0 \
@@ -27,4 +27,4 @@ python3 train_binary_classifier.py \
 --extraction_layer convlayers.conv21 \
 --feature_width 0 \
 --classifier logistic_regression \
---classifier_model_dir ${WORKSPACE}/MAFIA_classifiers/A_m6A_NoNorm_PassFail
+--classifier_model_dir ${WORKSPACE}/MAFIA_classifiers/A_m6A_NoNorm
