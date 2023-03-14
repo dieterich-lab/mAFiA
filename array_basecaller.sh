b871fb5d5af5265f46fc86d7675431310e7ca520
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-03-14 09:49:28 +0100

    job name change

--- array_basecaller.sh
+++ array_basecaller.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=2GB
 #SBATCH --verbose
-#SBATCH --job-name=basecaller
-#SBATCH --output=/home/achan/slurm/basecaller_%02a.out
+#SBATCH --job-name=array_basecaller
+#SBATCH --output=/home/achan/slurm/array_basecaller_%02a.out
 
 set -e -u
 
