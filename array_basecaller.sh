e641ab7cc83f7259acf722df82f840826ca3f570
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-03-15 13:11:48 +0100

    use only g3

--- array_basecaller.sh
+++ array_basecaller.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 #SBATCH --partition=gpu
-#SBATCH --exclude=gpu-g4-1,gpu-g2-1
+#SBATCH --nodelist=gpu-g3-1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --verbose
