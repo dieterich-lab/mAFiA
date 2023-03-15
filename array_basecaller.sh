735596401d9aeb7093c7e2956b699e845c943a23
Adrian <adrian.kaho.chan@gmail.com>
2023-03-15 10:37:58 +0100

    exclude more nodes

--- array_basecaller.sh
+++ array_basecaller.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 #SBATCH --partition=gpu
-#SBATCH --exclude=gpu-g4-1
+#SBATCH --exclude=gpu-g4-1,gpu-g2-1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=2GB
 #SBATCH --verbose
