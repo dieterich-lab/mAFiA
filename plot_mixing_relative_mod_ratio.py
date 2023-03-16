9a3cb7021855a0854865fb249a481f14192f8c27
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-03-16 10:11:21 +0100

    changed plotting script

--- plot_mixing_relative_mod_ratio.py
+++ plot_mixing_relative_mod_ratio.py
@@ -32,23 +32,37 @@ dfs_comp = [pd.read_csv(comp_df_file, sep='\t', index_col=0) for comp_df_file in
 
 P_VAL_THRESH = 1.0E-99
 COV_THRESH = 50
-df_base_thresh = df_base[(df_base['num_test_features']>=COV_THRESH) * (df_base['Pvalue']<P_VAL_THRESH)]
-dfs_comp_thresh = [
-    df_comp[(df_comp['num_test_features']>=COV_THRESH) * (df_comp['Pvalue']<P_VAL_THRESH)] for df_comp in dfs_comp
-]
-common_idx = list(set.intersection(set(df_base_thresh.index), *[set(this_df.index) for this_df in dfs_comp_thresh]))
+motifs = ['GGACA', 'GGACC', 'AGACT']
 
-x_vals = df_base_thresh.loc[common_idx]['mod_ratio'].values
+plt.figure(figsize=(15, 5))
+for subplot_ind, this_motif in enumerate(motifs):
+    df_base_thresh = df_base[
+            (df_base['num_test_features']>=COV_THRESH) *
+            (df_base['Pvalue']<P_VAL_THRESH) *
+            (df_base['motif']==this_motif)
+        ]
+    dfs_comp_thresh = [
+        df_comp[
+            (df_comp['num_test_features']>=COV_THRESH) *
+            (df_comp['Pvalue']<P_VAL_THRESH) *
+            (df_comp['motif']==this_motif)
+            ] for df_comp in dfs_comp
+    ]
+    common_idx = list(set.intersection(set(df_base_thresh.index), *[set(this_df.index) for this_df in dfs_comp_thresh]))
 
-plt.figure(figsize=(8, 8))
-for (df, ds) in zip(dfs_comp_thresh, comp_datasets):
-    y_vals = df.loc[common_idx]['mod_ratio'].values
-    plt.plot(x_vals, y_vals, 'o', mfc='none', label=ds)
-plt.xlim([-0.05, 1.05])
-plt.ylim([-0.05, 1.05])
-plt.xlabel('WT value', fontsize=15)
-plt.ylabel('ONT mod. ratio', fontsize=15)
-plt.legend(loc='upper left', fontsize=10)
+    x_vals = df_base_thresh.loc[common_idx]['mod_ratio'].values
+
+    plt.subplot(1, 3, subplot_ind+1)
+    for (df, ds) in zip(dfs_comp_thresh, comp_datasets):
+        y_vals = df.loc[common_idx]['mod_ratio'].values
+        plt.plot(x_vals, y_vals, 'o', mfc='none', label=' '.join(ds.split('-')[1:3]))
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('WT mod. ratio', fontsize=15)
+    if subplot_ind==0:
+        plt.ylabel('Mixture mod. ratio', fontsize=15)
+    plt.legend(loc='upper left', fontsize=10)
+    plt.title(this_motif, fontsize=15)
 
 # plt.title(r'{} sites with ONT coverage$\geq${}'.format(df_motif.shape[0], coverage_thresh) + '\nCorrelation {:.2f}'.format(corr), fontsize=15)
 # plt.subplots_adjust(top=0.8)
