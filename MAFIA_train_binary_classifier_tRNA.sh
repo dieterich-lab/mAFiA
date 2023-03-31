8d3cf2f03fc90ddb81c86364ce0b534f8ec8d355
Adrian <adrian.kaho.chan@gmail.com>
2023-03-31 11:35:47 +0200

    small changes to path in train script

--- MAFIA_train_binary_classifier_tRNA.sh
+++ MAFIA_train_binary_classifier_tRNA.sh
@@ -13,11 +13,12 @@ set -e -u
 
 cd ${HOME}/git/MAFIA
 WORKSPACE=/beegfs/prj/tRNA_Berlin/newBatchDec2022_Spombe
+OUTDIR=${WORKSPACE}/achan/parasail_filtered_classifier_models
 
 python3 tRNA_train_binary_classifier.py \
---wt_bam_file ${WORKSPACE}/achan/mapping/tRNA_Q.bam.sorted \
+--wt_bam_file ${WORKSPACE}/achan/mapping/parasail/tRNA_Q.bam.filtered.sorted \
 --wt_fast5_dir ${WORKSPACE}/agg_fast5_pass/tRNA_Q \
---ivt_bam_file ${WORKSPACE}/achan/mapping/tRNA_IVT.bam.sorted \
+--ivt_bam_file ${WORKSPACE}/achan/mapping/parasail/tRNA_IVT.bam.filtered.sorted \
 --ivt_fast5_dir ${WORKSPACE}/agg_fast5_pass/tRNA_IVT \
 --ref_file ${WORKSPACE}/S.pombe_mature_tRNAs_adapters_nuclear_and_mt.fasta \
 --mod_file ${WORKSPACE}/S_pombe_Q_only.tsv \
@@ -25,5 +26,5 @@ python3 tRNA_train_binary_classifier.py \
 --min_coverage 0 \
 --model_path ${HOME}/pytorch_models/tRNA_IVT/tRNA_IVT-epoch29.torch \
 --classifier logistic_regression \
---classifier_model_dir ${WORKSPACE}/achan/classifier_models \
---outfile ${WORKSPACE}/achan/classifier_models/logreg_auc.tsv
+--classifier_model_dir ${OUTDIR} \
+--outfile ${OUTDIR}/logreg_auc.tsv
