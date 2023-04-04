4bdcfba89f05bccd98e7e19f42168116a3de97e8
Adrian <adrian.kaho.chan@gmail.com>
2023-04-04 09:37:16 +0200

    changed paths in train script

--- MAFIA_train_binary_classifier_tRNA.sh
+++ MAFIA_train_binary_classifier_tRNA.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=120GB
 #SBATCH --nodes=1
 #SBATCH --verbose
-#SBATCH --output=/home/achan/slurm/MAFIA_train_binary_classifier_tRNA.out
+#SBATCH --output=/home/achan/slurm/MAFIA_train_binary_classifier_tRNA_%A.out
 
 eval "$(conda shell.bash hook)"
 conda activate MAFIA
@@ -13,17 +13,17 @@ set -e -u
 
 cd ${HOME}/git/MAFIA
 WORKSPACE=/beegfs/prj/tRNA_Berlin/newBatchDec2022_Spombe
-OUTDIR=${WORKSPACE}/achan/parasail_filtered_classifier_models
+OUTDIR=${WORKSPACE}/achan/rev_filtered_classifier_models
 
 python3 tRNA_train_binary_classifier.py \
---wt_bam_file ${WORKSPACE}/achan/mapping/parasail/tRNA_Q.bam.filtered.sorted \
+--wt_bam_file ${WORKSPACE}/achan/mapping/tRNA_Q.bam \
 --wt_fast5_dir ${WORKSPACE}/agg_fast5_pass/tRNA_Q \
---ivt_bam_file ${WORKSPACE}/achan/mapping/parasail/tRNA_IVT.bam.filtered.sorted \
+--ivt_bam_file ${WORKSPACE}/achan/mapping/tRNA_IVT.bam \
 --ivt_fast5_dir ${WORKSPACE}/agg_fast5_pass/tRNA_IVT \
 --ref_file ${WORKSPACE}/S.pombe_mature_tRNAs_adapters_nuclear_and_mt.fasta \
 --mod_file ${WORKSPACE}/S_pombe_Q_only.tsv \
 --max_num_reads -1 \
---min_coverage 0 \
+--min_coverage 10 \
 --model_path ${HOME}/pytorch_models/tRNA_IVT/tRNA_IVT-epoch29.torch \
 --classifier logistic_regression \
 --classifier_model_dir ${OUTDIR} \
