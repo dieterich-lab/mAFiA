64787b1fdb51d2bb7e21a5780a61b3a600a965ff
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-04-25 17:15:48 +0200

    update nodelist

--- array_basecaller.sh
+++ array_basecaller.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --partition=gpu
 #SBATCH --exclude=gpu-g4-1
-#SBATCH --nodelist=gpu-g3-1
+#SBATCH --nodelist=gpu-g3-1,gpu-g2-1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --verbose
