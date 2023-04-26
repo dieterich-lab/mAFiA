a24ea1e7f41bed2c7932d71b006b7fed2e15ce86
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-04-26 09:35:12 +0200

    reorganize workflow folder by dataset

--- MAFIA_prepare_mRNA_test_data.sh
+++ MAFIA_prepare_mRNA_test_data.sh
@@ -1,4 +1,3 @@
-WORKSPACE=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Adrian
 REF=${HOME}/Data/genomes/GRCh38_96.fa
 ARCH=${HOME}/git/renata/rnaarch
 MODEL=${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch
@@ -11,8 +10,8 @@ MODEL=${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.t
 #FAST5_DIR=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Adrian/fast5/HEK293_IVT_2/fast5_pass
 
 ### new HEK293 ######################################################################################################################
-DATASET=100_WT_0_IVT_RTA
-#DATASET=0_WT_100_IVT_RTA
+#DATASET=100_WT_0_IVT_RTA
+DATASET=0_WT_100_IVT_RTA
 FAST5_DIR=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Isabel/20230419_HEK293_WT_IVT_Mix/${DATASET}/*/fast5_*
 
 #####################################################################################################################################
@@ -42,44 +41,42 @@ FAST5_DIR=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Isabel/20230419_HEK293_WT_
 #FAST5_DIR=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Adrian/fast5/HEK293T-WT-Mettl3-Mix/HEK293T-WT-rep3/fast5
 
 #####################################################################################################################################
-
+WORKSPACE=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Adrian/${DATASET}
 mkdir -p ${WORKSPACE}
-FASTA=${WORKSPACE}/basecall/${DATASET}.fasta
-SAM=${WORKSPACE}/mapping/${DATASET}.sam
-BAM=${SAM//.sam/.bam}
+
+FASTA=${WORKSPACE}/basecall.fasta
+SAM=${WORKSPACE}/genome_mapped.sam
+BAM=${SAM//.sam/_q50.bam}
 
 source ${HOME}/git/renata/virtualenv/bin/activate
 
 ### basecall large number of reads ###
-FILENAME_PREFIX=${DATASET}_fast5_paths_part
-ls -1 ${FAST5_DIR}/*.fast5 > ${WORKSPACE}/${DATASET}_fast5_paths_all
-split -l10 -d ${WORKSPACE}/${DATASET}_fast5_paths_all ${WORKSPACE}/${FILENAME_PREFIX}
-
-#NUM_STRAND_FILES=`ls ${WORKSPACE}/${DATASET}_fast5_paths_part* | wc -l`
-#sbatch --array=0-$((${NUM_STRAND_FILES}-1)) --export=ALL,WORKSPACE=${WORKSPACE},FAST5_DIR=${FAST5_DIR},FILENAME_PREFIX=${FILENAME_PREFIX},FASTA=${FASTA},ARCH=${ARCH},MODEL=${MODEL} ${HOME}/git/MAFIA/array_basecaller.sh
+FILENAME_PREFIX=fast5_paths_part
+ls -1 ${FAST5_DIR}/*.fast5 > ${WORKSPACE}/fast5_paths_all
+split -l10 -d ${WORKSPACE}/fast5_paths_all ${WORKSPACE}/${FILENAME_PREFIX}
 
 NUM_ARRAYS=""
-for f in ${WORKSPACE}/${DATASET}_fast5_paths_part*; do ff=${f##*part}; NUM_ARRAYS+="${ff},"; done
+for f in ${WORKSPACE}/fast5_paths_part*; do ff=${f##*part}; NUM_ARRAYS+="${ff},"; done
 NUM_ARRAYS=${NUM_ARRAYS%,*}
-sbatch --array=${NUM_ARRAYS} --export=ALL,WORKSPACE=${WORKSPACE},FILENAME_PREFIX=${FILENAME_PREFIX},FASTA=${FASTA},ARCH=${ARCH},MODEL=${MODEL} ${HOME}/git/MAFIA/array_basecaller.sh
+sbatch --array=${NUM_ARRAYS} --export=ALL,WORKSPACE=${WORKSPACE},FILENAME_PREFIX=${FILENAME_PREFIX},ARCH=${ARCH},MODEL=${MODEL} ${HOME}/git/MAFIA/array_basecaller.sh
 
 #for f in ${FASTA}+([0-9]); do echo $f; grep '>' $f | wc -l; done
 
-cat ${FASTA}+([0-9]) > ${FASTA}_merged
+cat ${WORKSPACE}/part*.fasta > ${WORKSPACE}/merged.fasta
 
 #### align to genome ###
 module purge
 module load minimap2
-minimap2 --secondary=no -ax splice -uf -k14 -t 36 --cs ${REF} ${FASTA}_merged > ${SAM}
+minimap2 --secondary=no -ax splice -uf -k14 -t 36 --cs ${REF} ${WORKSPACE}/merged.fasta > ${SAM}
 
 ### check stats and accuracy ###
 samtools flagstats ${SAM}
 ${HOME}/git/renata/accuracy.py ${SAM} ${REF}
 
 #### Convert to BAM and index ###
-samtools view -bST ${REF} ${SAM} | samtools sort - > ${BAM}
+samtools view -bST -q50 ${REF} ${SAM} | samtools sort - > ${BAM}
 samtools index ${BAM}
 
 ### clean up ###
-rm ${FASTA}+([0-9])
-rm ${WORKSPACE}/${DATASET}_fast5_paths_all
+rm ${WORKSPACE}/part*.fasta
+rm ${WORKSPACE}/fast5_paths_all
--- array_basecaller.sh
+++ array_basecaller.sh
@@ -15,14 +15,14 @@ source ${HOME}/git/renata/virtualenv/bin/activate
 
 printf -v PART '%02d' "${SLURM_ARRAY_TASK_ID}"
 LIST_FILENAMES=${WORKSPACE}/${FILENAME_PREFIX}${PART}
-OUTPUT=${FASTA}${PART}
+OUTPUT=${WORKSPACE}/part${PART}.fasta
 echo "Running basecaller on ${LIST_FILENAMES}"
 
 python3 ${HOME}/git/renata/basecall_viterbi.py \
 --list_filenames ${LIST_FILENAMES} \
 --arch ${ARCH} \
 --model ${MODEL} \
---batchsize 128 \
+--batchsize 1024 \
 --decoder viterbi \
 > ${OUTPUT}
 
