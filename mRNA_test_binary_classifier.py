f30d514ad75de8419950c52dd94b51f3b5dc5667
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-04-26 15:26:34 +0200

    fixed bam mistake

--- mRNA_test_binary_classifier.py
+++ mRNA_test_binary_classifier.py
@@ -67,9 +67,9 @@ for record in SeqIO.parse(ref_file, 'fasta'):
 
 ### collect reads ###
 test_bam = pysam.AlignmentFile(test_bam_file, 'rb')
-test_f5_paths = iglob(os.path.join(test_fast5_dir, '*.fast5'), recursive=True)
+test_f5_paths = list(iglob(os.path.join(test_fast5_dir, '*.fast5'), recursive=True))
 print('Parsing test fast5 files {}'.format(test_f5_paths), flush=True)
-test_index_read_ids = index_fast5_files(test_f5_paths, test_bam_file)
+test_index_read_ids = index_fast5_files(test_f5_paths, test_bam)
 print('{} test reads indexed'.format(len(test_index_read_ids)), flush=True)
 
 ### load model, device ###
