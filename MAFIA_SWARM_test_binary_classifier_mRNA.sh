e56f610c3b362f17ef9e19365fe1bc7135d4d02b
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-04-26 15:01:25 +0200

    fixed slurm task id %02

--- MAFIA_SWARM_test_binary_classifier_mRNA.sh
+++ MAFIA_SWARM_test_binary_classifier_mRNA.sh
@@ -6,23 +6,25 @@
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --verbose
 #SBATCH --job-name=MAFIA_SWARM_test_binary_classifier_mRNA
-#SBATCH --output=/home/achan/slurm/MAFIA_SWARM_test_binary_classifier_mRNA_%A_%a.out
+#SBATCH --output=/home/achan/slurm/MAFIA_SWARM_test_binary_classifier_mRNA_%A_%02a.out
 
 eval "$(conda shell.bash hook)"
 conda activate MAFIA
 
 set -e -u -f
 
+printf -v PART '%02d' "${SLURM_ARRAY_TASK_ID}"
+
 python3 ${HOME}/git/MAFIA/mRNA_test_binary_classifier.py \
 --test_bam_file ${BAM} \
 --test_fast5_dir ${FAST5_DIR} \
 --ref_file ${REF} \
---mod_file ${MOD_FILE}.part${SLURM_ARRAY_TASK_ID} \
+--mod_file ${MOD_FILE}.part${PART} \
 --max_num_reads 1000 \
 --min_coverage 50 \
 --backbone_model_path ${BACKBONE_MODEL} \
 --extraction_layer ${EXTRACTION_LAYER} \
 --classifier logistic_regression \
 --classifier_model_dir ${CLASSIFIER_MODEL_DIR} \
---outfile ${OUTFILE}.part${SLURM_ARRAY_TASK_ID} \
+--outfile ${OUTFILE}.part${PART} \
 --output_mod_probs
