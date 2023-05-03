53cea5cfee9e9ff6e4cdb3c9a4fdc568cd016bb3
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-05-03 15:42:16 +0200

    small updates

--- generate_random_ligation_fasta.py
+++ generate_random_ligation_fasta.py
@@ -19,12 +19,15 @@ def add_block(base_list, add_list, base_names, add_names):
 
 max_blocks = 7
 ref_dir = '/home/adrian/Data/TRR319_RMaP/Project_BaseCalling/Adrian/reference'
-# base_fasta = os.path.join(ref_dir, 'WUE_splint_test_w_splint.fasta')
-# out_fasta = os.path.join(ref_dir, 'splint_variations_max_blocks_{}.fasta'.format(max_blocks))
+### WUE batch 1 ###
+base_fasta = os.path.join(ref_dir, 'WUE_batch1_w_splint.fasta')
+out_fasta = os.path.join(ref_dir, 'WUE_batch1_blocks{}.fasta'.format(max_blocks))
+### WUE batch 2 ###
+# base_fasta = os.path.join(ref_dir, 'WUE_batch2_w_splint.fasta')
+# out_fasta = os.path.join(ref_dir, 'WUE_batch2_blocks{}.fasta'.format(max_blocks))
+### Isabel all 6 ###
 # base_fasta = os.path.join(ref_dir, 'Top-6-GLORI_from_48-oligo-list.fasta')
 # out_fasta = os.path.join(ref_dir, 'top6_random_permutation_max_blocks_{}.fasta'.format(max_blocks))
-base_fasta = os.path.join(ref_dir, 'WUE_batch2_w_splint.fasta')
-out_fasta = os.path.join(ref_dir, 'WUE_batch2_max_blocks_{}.fasta'.format(max_blocks))
 
 base_sequences = []
 for record in SeqIO.parse(base_fasta, 'fasta'):
