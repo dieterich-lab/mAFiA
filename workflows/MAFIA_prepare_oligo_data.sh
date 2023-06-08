036ea76aeb4235013ae896d86ba1cbbf1cf1bfa0
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-06-08 10:25:40 +0200

    input args for prep script

--- workflows/MAFIA_prepare_oligo_data.sh
+++ workflows/MAFIA_prepare_oligo_data.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-PRJ_DIR=/prj/TRR319_RMaP/Project_BaseCalling/Adrian
-
-ARCH=${HOME}/git/renata/rnaarch
-MODEL=${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch
-FILTER_SCORE=70
+while getopts o:r:m:h:l: flag
+do
+  case "${flag}" in
+    o) ORIG=${OPTARG};;
+    r) RUN=${OPTARG};;
+    m) MOD=${OPTARG};;
+    h) HOMOPOLYMER=${OPTARG};;
+    l) LOC=${OPTARG};;
+  esac
+done
 
 ########################################################################################################################
 ### WUE batches 1-2 ####################################################################################################
 ########################################################################################################################
-HOMOPOLYMER=0
-
-ORIG=WUE
-RUN=batch2
-MOD=A
+#HOMOPOLYMER=0
+#
+#ORIG=WUE
+#RUN=batch2
+#MOD=A
 
 ########################################################################################################################
 ### ISA runs 1-3 #######################################################################################################
@@ -39,17 +44,26 @@ MOD=A
 #FAST5_DIR=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/${DATASET}/fast5
 
 ########################################################################################################################
-
+PRJ_DIR=/prj/TRR319_RMaP/Project_BaseCalling/Adrian
+ARCH=${HOME}/git/renata/rnaarch
+MODEL=${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch
+FILTER_SCORE=70
 REF=${PRJ_DIR}/reference/${ORIG}_oligo_ref_${RUN}.fasta
 DATASET=${ORIG}_${RUN}_${MOD}
-
-WORKSPACE=${PRJ_DIR}/${DATASET}
-cd ${WORKSPACE}
-
+WORKSPACE=${PRJ_DIR}/oligo/${DATASET}
 FAST5_DIR=${WORKSPACE}/fast5
 FASTA=${WORKSPACE}/renata.fasta
 SAM=${WORKSPACE}/spomelette.sam
 LIGATION_REF=${WORKSPACE}/ligation_ref.fasta
+
+### softlink fast5 files ###
+mkdir -p ${FAST5_DIR} && cd "$_"
+echo "Creating softlinks from ${LOC}"
+for f in ${LOC}/*/fast5_*/*.fast5
+do
+  ln -s $f
+done
+cd ${WORKSPACE}
 
 #### basecall with Rodan IVT ###
 #deactivate
@@ -86,12 +100,13 @@ FILTERED_SAM=${SAM/.sam/_q${FILTER_SCORE}.sam}
 samtools view -h -q${FILTER_SCORE} ${SAM} > ${FILTERED_SAM}
 
 ### check read num and accuracy ###
-samtools flagstats ${FILTERED_SAM}
-${HOME}/git/renata/accuracy.py ${FILTERED_SAM} ${LIGATION_REF}
+echo "Quality control"
+samtools flagstats ${FILTERED_SAM} > ${WORKSPACE}/flagstats.txt
+${HOME}/git/renata/accuracy.py ${FILTERED_SAM} ${LIGATION_REF} > ${WORKSPACE}/flagstats.txt
 
 
 #### Convert to BAM ###
-echo "Converting ${BAM}"
 BAM=${FILTERED_SAM//.sam/.bam}
+echo "Converting to ${BAM}"
 samtools view -bST ${LIGATION_REF} ${FILTERED_SAM} | samtools sort - > ${BAM}
 samtools index ${BAM}
