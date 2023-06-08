93073bb7dc090fe82f9afba08c9a621652a80e9e
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-06-08 12:07:43 +0200

    rename and clean up train shell script

---
+++ workflows/train_MAFIA_with_oligo.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#SBATCH --partition=gpu
+#SBATCH --nodelist=gpu-g3-1
+#SBATCH --mem=120GB
+#SBATCH --nodes=1
+#SBATCH --verbose
+#SBATCH --output=/home/achan/slurm/train_MAFIA_%A.out
+
+eval "$(conda shell.bash hook)"
+#eval "$(/home/achan/miniconda3/condabin/conda shell.bash hook)"
+conda activate MAFIA
+set -e -f
+cd ${HOME}/git/MAFIA
+
+DATA_DIR=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Adrian/oligo
+
+############################################################################################
+### Training data ##########################################################################
+############################################################################################
+### ISA ###
+TRAIN_DATASET=ISA_runs1-3
+
+### WUE ###
+#TRAIN_DATASET=WUE_batch1_batch2
+
+############################################################################################
+### Backbone settings ######################################################################
+############################################################################################
+BACKBONE_MODEL=${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch
+EXT_LAYER=convlayers.conv21
+CLASSIFIER=logistic_regression
+SCALER=MaxAbs
+############################################################################################
+############################################################################################
+
+UNM_BAM=${DATA_DIR}/${TRAIN_DATASET}_A/spomelette_q70.bam
+UNM_FAST5=${DATA_DIR}/${TRAIN_DATASET}_A/fast5
+MOD_BAM=${DATA_DIR}/${TRAIN_DATASET}_m6A/spomelette_q70.bam
+MOD_FAST5=${DATA_DIR}/${TRAIN_DATASET}_m6A/fast5
+REF=${DATA_DIR}/${TRAIN_DATASET}_A_m6A/ligation_ref.fa
+OUTDIR=${DATA_DIR}/MAFIA_classifiers/${TRAIN_DATASET}
+
+python3 -u oligo_train_binary_classifier.py \
+--unm_bam_file ${UNM_BAM} \
+--unm_fast5_dir ${UNM_FAST5} \
+--mod_bam_file ${MOD_BAM} \
+--mod_fast5_dir ${MOD_FAST5} \
+--ref_file ${REF} \
+--backbone_model_path ${BACKBONE_MODEL} \
+--scaler ${SCALER} \
+--min_coverage 10 \
+--classifier_model_dir ${OUTDIR}
