edec962d9c98a24097d3e88d8f2368240d360c0c
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-06-08 11:04:08 +0200

    bash instead of sbatch

--- workflows/prepare_multiple_oligo_datasets.sh
+++ workflows/prepare_multiple_oligo_datasets.sh
@@ -6,7 +6,7 @@ DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/db_ISA_run2.tsv
   read -r
   while read -r o r m h l
   do
-    echo "o=$o, r=$r, m=$m, h=$h, l=$l"
-    sbatch ${HOME}/git/MAFIA/workflows/prepare_single_oligo_dataset.sh -o=$o -r=$r -m=$m -h=$h -l=$l
+    echo "ORIG=$o, RUN=$r, MOD=$m, HOMOPOLYMER=$h, LOC=$l"
+    bash ${HOME}/git/MAFIA/workflows/prepare_single_oligo_dataset.sh -o=$o -r=$r -m=$m -h=$h -l=$l &
   done
 } < $DB
