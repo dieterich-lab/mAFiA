8872fddeed94bab74afa542a34ea4c7ebb0bef5f
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-06-08 11:08:52 +0200

    fixed = error in args

--- workflows/prepare_multiple_oligo_datasets.sh
+++ workflows/prepare_multiple_oligo_datasets.sh
@@ -7,6 +7,6 @@ DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/db_ISA_run2.tsv
   while read -r o r m h l
   do
     echo "ORIG=$o, RUN=$r, MOD=$m, HOMOPOLYMER=$h, LOC=$l"
-    bash ${HOME}/git/MAFIA/workflows/prepare_single_oligo_dataset.sh -o=$o -r=$r -m=$m -h=$h -l=$l &
+    bash ${HOME}/git/MAFIA/workflows/prepare_single_oligo_dataset.sh -o "$o" -r "$r" -m "$m" -h "$h" -l "$l" &
   done
 } < $DB
