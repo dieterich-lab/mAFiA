dc27de56bdcec6d422335389f2f8c80337452122
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-06-13 13:17:31 +0200

    added mod to test dataset

--- workflows/multi_test_MAFIA_oligo.sh
+++ workflows/multi_test_MAFIA_oligo.sh
@@ -2,7 +2,7 @@
 
 for TRAIN_DATASET in ISA WUE ISA-WUE
 do
-  for TEST_DATASET in ISA WUE ISA-WUE
+  for TEST_DATASET in ISA_A ISA_m6A WUE_A WUE_m6A ISA-WUE_A ISA-WUE_m6A
   do
     echo "Train - ${TRAIN_DATASET}; Test - ${TEST_DATASET}"
     sbatch --export=ALL,TRAIN_DATASET=${TRAIN_DATASET},TEST_DATASET=${TEST_DATASET} ${HOME}/git/MAFIA/workflows/test_MAFIA_oligo.sh
