c51f95a1ba1354484918c416ee22024aeef0b109
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-06-13 09:42:51 +0200

    check empty queue

--- oligo_test_binary_classifier.py
+++ oligo_test_binary_classifier.py
@@ -31,6 +31,7 @@ if __name__ == "__main__":
 
     for process in processes:
         process.start()
+        print(f'Empty queue: {queue.empty()}')
     women_containers = []
     while not queue.empty():
         print(queue, flush=True)
