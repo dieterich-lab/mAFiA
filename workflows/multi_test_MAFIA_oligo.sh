b592d3e35ff3a5b33395c95f42e11e9d9acb9f5a
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-06-14 15:55:45 +0200

    added more datasets to loop

--- workflows/multi_test_MAFIA_oligo.sh
+++ workflows/multi_test_MAFIA_oligo.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-for TRAIN_DATASET in ISA WUE ISA-WUE
+#for TRAIN_DATASET in ISA WUE ISA-WUE
+for TRAIN_DATASET in ISA-WUE
 do
-  for TEST_DATASET in ISA_A ISA_m6A WUE_A WUE_m6A ISA-WUE_A ISA-WUE_m6A
+#  for TEST_DATASET in ISA_A ISA_m6A WUE_A WUE_m6A ISA-WUE_A ISA-WUE_m6A
+  for TEST_DATASET in ISA_run4_M4M5 ISA_run4_M4M5star ISA_run4_M4starM5 ISA_run4_M4starM5star
   do
     echo "Train - ${TRAIN_DATASET}; Test - ${TEST_DATASET}"
     sbatch --export=ALL,TRAIN_DATASET=${TRAIN_DATASET},TEST_DATASET=${TEST_DATASET} ${HOME}/git/MAFIA/workflows/test_MAFIA_oligo.sh
