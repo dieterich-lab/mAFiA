0766deb185d674b23306811376d3446af28328ec
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-06-27 09:22:49 +0200

    added WUE batch 3 in test

--- workflows/multi_test_MAFIA_oligo.sh
+++ workflows/multi_test_MAFIA_oligo.sh
@@ -4,7 +4,8 @@
 for TRAIN_DATASET in ISA-WUE
 do
 #  for TEST_DATASET in ISA_A ISA_m6A WUE_A WUE_m6A ISA-WUE_A ISA-WUE_m6A
-  for TEST_DATASET in ISA_run4_M4M5 ISA_run4_M4M5star ISA_run4_M4starM5 ISA_run4_M4starM5star
+#  for TEST_DATASET in ISA_run4_M4M5 ISA_run4_M4M5star ISA_run4_M4starM5 ISA_run4_M4starM5star
+for TEST_DATASET in WUE_batch3_AB WUE_batch3_BA WUE_batch3_ABBA
   do
     echo "Train - ${TRAIN_DATASET}; Test - ${TEST_DATASET}"
     sbatch --export=ALL,TRAIN_DATASET=${TRAIN_DATASET},TEST_DATASET=${TEST_DATASET} ${HOME}/git/MAFIA/workflows/test_MAFIA_oligo.sh
