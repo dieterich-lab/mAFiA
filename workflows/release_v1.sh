ae9c886b4abb5075b173f87f3c1db22ac30a04a9
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-07-25 11:03:58 +0200

    renamed paths

--- workflows/release_v1.sh
+++ workflows/release_v1.sh
@@ -1,11 +1,19 @@
 conda activate MAFIA
 
 workspace="/prj/TRR319_RMaP/Project_BaseCalling/Adrian/release_v1"
-backbone="${workspace}/models/backbone.torch"
-classifier="${workspace}/models/mAFiA"
-fast5dir="${workspace}/data/fast5_chrX"
-basecall="${workspace}/output/basecall.fasta"
-mkdir -p "${workspace}/output"
+models="${workspace}/models"
+data="${workspace}/data"
+output="${workspace}/output"
+
+backbone="${models}/backbone.torch"
+classifier="${models}/mAFiA"
+fast5dir="${data}/fast5_chrX"
+ref="${data}/GRCh38_96.X.fa"
+mod="${data}/GLORI_chrX.bed"
+
+mkdir -p "${output}"
+basecall="${output}/rodan.fasta"
+bam="${output}/minimap.q50.bam"
 
 ########################################################################################################################
 ### basecall ###########################################################################################################
@@ -21,9 +29,6 @@ python3 ${HOME}/git/MAFIA/rodan_viterbi.py \
 ########################################################################################################################
 #### align #############################################################################################################
 ########################################################################################################################
-ref="${workspace}/data/GRCh38_96.X.fa"
-bam="${workspace}/output/aligned_filtered_q50.bam"
-
 module purge
 module load minimap2
 
@@ -36,8 +41,6 @@ samtools index ${bam}
 ########################################################################################################################
 #### mAFiA #############################################################################################################
 ########################################################################################################################
-mod="${workspace}/data/GLORI_chrX.bed"
-
 python3 ${HOME}/git/MAFIA/test_mAFiA.py \
 --test_bam_file ${bam} \
 --test_fast5_dir ${fast5dir} \
@@ -48,4 +51,4 @@ python3 ${HOME}/git/MAFIA/test_mAFiA.py \
 --backbone_model_path ${backbone} \
 --classifier_model_dir ${classifier} \
 --mod_prob_thresh 0.5 \
---out_dir "${workspace}/output" &
+--out_dir ${output} &
