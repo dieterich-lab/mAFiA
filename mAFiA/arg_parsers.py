8383d1cc809276643fce5034b0158cc4490c4471
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-07-27 14:14:15 +0200

    remove unused arg

--- mAFiA/arg_parsers.py
+++ mAFiA/arg_parsers.py
@@ -47,4 +47,3 @@ class mRNATestArgsParser(TestArgsParser):
         self.add_argument('--features_file', default=None)
         self.add_argument('--mod_file')
         self.add_argument('--mod_prob_thresh', type=float, default=0.5)
-        self.add_argument('--output_mod_probs', action='store_true')
