ccf946ffcbdd86af88016fd6b271cb5a4134b3aa
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-09-19 14:59:13 +0200

    added conda bash hook

--- workflows/prepare_Gm_oligo_dataset.sh
+++ workflows/prepare_Gm_oligo_dataset.sh
@@ -28,26 +28,27 @@ arch=${HOME}/git/renata/rnaarch
 backbone=${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch
 
 ### softlink to fast5 pass files ###
-fast5pass=${workspace}/${dataset}/fast5_pass
-mkdir -p ${fast5pass}
-cd ${fast5pass}
-for f in ${fast5dir}/*pass*.fast5; do ln -s $f; done
+#fast5pass=${workspace}/${dataset}/fast5_pass
+#mkdir -p ${fast5pass}
+#cd ${fast5pass}
+#for f in ${fast5dir}/*pass*.fast5; do ln -s $f; done
 
 #### basecall with Rodan IVT ###
-source ${HOME}/git/renata/virtualenv/bin/activate
-
-echo "Basecalling ${fast5pass}"
-srun --partition=gpu --gres=gpu:turing:1 --cpus-per-task=8 --mem-per-cpu=8GB \
-python3 -u ${HOME}/git/renata/basecall_viterbi.py \
---fast5dir ${fast5pass} \
---arch ${arch} \
---model ${backbone} \
---batchsize 4096 \
---decoder viterbi \
-> ${fasta}
+#source ${HOME}/git/renata/virtualenv/bin/activate
+#
+#echo "Basecalling ${fast5pass}"
+#srun --partition=gpu --gres=gpu:turing:1 --cpus-per-task=8 --mem-per-cpu=8GB \
+#python3 -u ${HOME}/git/renata/basecall_viterbi.py \
+#--fast5dir ${fast5pass} \
+#--arch ${arch} \
+#--model ${backbone} \
+#--batchsize 4096 \
+#--decoder viterbi \
+#> ${fasta}
 
 ### align with spomelette ###
 deactivate
+eval "$(/home/achan/miniconda3/condabin/conda shell.bash hook)"
 conda activate MAFIA
 
 echo "Basecalling finished. Now aligning ${fasta} to ${oligo_ref}"
