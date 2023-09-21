b46d27d0389551b42ff2b33f2bde70eb5160a853
Adrian Chan <adrian.kaho.chan@gmail.com>
2023-09-21 11:48:19 +0200

    uncomment working part

--- workflows/array_test_Gmorah.sh
+++ workflows/array_test_Gmorah.sh
@@ -19,13 +19,13 @@ batch=${SLURM_ARRAY_TASK_ID}
 
 echo "Running Gmorah on ${challenge}, ${test_ds}, ${batch}"
 
-#set -e -u
-#
-#python3 ${HOME}/git/Gmorah/mAFiA/test_mAFiA.py \
-#--ref_file ${prj}/Gm/References/test_reference.fasta \
-#--backbone_model_path ${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch \
-#--classifier_model_dir ${prj}/Adrian/Gm/Gmorah_models/combined \
-#--bam_file ${prj}/Adrian/Gm/Test/${test_ds}/minimap.bam \
-#--fast5_dir ${prj}/Adrian/Gm/Test/${test_ds}/fast5_batch${batch} \
-#--out_dir ${prj}/Adrian/Gm/Test/${test_ds}/Gmorah_batch${batch} \
-#--mod_file ${prj}/Gm/References/Gm_test_sites.bed
+set -e -u
+
+python3 ${HOME}/git/Gmorah/mAFiA/test_mAFiA.py \
+--ref_file ${prj}/Gm/References/test_reference.fasta \
+--backbone_model_path ${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch \
+--classifier_model_dir ${prj}/Adrian/Gm/Gmorah_models/combined \
+--bam_file ${prj}/Adrian/Gm/Test/${test_ds}/minimap.bam \
+--fast5_dir ${prj}/Adrian/Gm/Test/${test_ds}/fast5_batch${batch} \
+--out_dir ${prj}/Adrian/Gm/Test/${test_ds}/Gmorah_batch${batch} \
+--mod_file ${prj}/Gm/References/Gm_test_sites.bed
