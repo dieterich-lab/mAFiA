078677e457731a4f26d17a2ab48b704deeb6cab0
Adrian <adrian.kaho.chan@gmail.com>
2023-11-20 10:46:12 +0100

    changed single mRNA test workflow

--- workflows/test_mAFiA_single.sh
+++ workflows/test_mAFiA_single.sh
@@ -3,19 +3,19 @@
 #SBATCH --exclude=gpu-g4-1
 #SBATCH --gres=gpu:turing:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=120GB
+#SBATCH --mem=500GB
 #SBATCH --verbose
 #SBATCH --job-name=test_mAFiA
 #SBATCH --output=/home/achan/slurm/test_mAFiA_%A.out
 
-workspace=/prj/TRR319_RMaP/Project_B01/Adrian/JK_HEK293_DMSO_merged
-bam=${workspace}/genome_filtered_q50.bam
-fast5_dir=${workspace}/fast5
+workspace=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/HEK293/100_WT_0_IVT
+bam=${workspace}/chrX_q50.bam
+fast5_dir=${workspace}/fast5_chrX
 ref=/biodb/genomes/homo_sapiens/GRCh38_102/GRCh38_102.fa
-mod=/prj/TRR319_RMaP/Project_B01/Adrian/possible_IR_sites_edge50.tsv
+mod=/home/achan/Data/GLORI/bed_files/GLORI_chrX_ref5mer.tsv
 backbone=${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch
-classifiers=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/release_v1/models/classifiers
-output=${workspace}/mAFiA
+classifiers=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/MAFIA_classifiers/DRACH
+output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/results/mAFiA_deep_chrX
 
 source ${HOME}/git/mAFiA/mafia-venv/bin/activate
 
@@ -24,8 +24,7 @@ test_mAFiA \
 --fast5_dir ${fast5_dir} \
 --ref_file ${ref} \
 --mod_file ${mod} \
---min_coverage 20 \
---max_num_reads 1000 \
+--min_coverage 10 \
 --backbone_model_path ${backbone} \
 --classifier_model_dir ${classifiers} \
 --mod_prob_thresh 0.5 \
