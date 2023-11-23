18c514dfcdb0bc20dd342a13d99314e6c5adbd1e
Adrian <adrian.kaho.chan@gmail.com>
2023-11-23 15:13:33 +0100

    Changes to be committed: 	modified:   misc/generate_all_DRACH_sites_from_ref.py 	modified:   workflows/test_mAFiA_single.sh

--- misc/generate_all_DRACH_sites_from_ref.py
+++ misc/generate_all_DRACH_sites_from_ref.py
@@ -1,8 +1,12 @@
+import os
 from Bio import SeqIO
+from Bio.Seq import Seq
 import re
+from tqdm import tqdm
 import pandas as pd
 
-ref_file = '/home/adrian/Data/GRCh38_102/GRCh38_102.fa'
+# ref_file = '/home/adrian/Data/GRCh38_102/GRCh38_102.fa'
+ref_file = '/biodb/genomes/homo_sapiens/GRCh38_102/GRCh38_102.fa'
 ref = {}
 for record in SeqIO.parse(ref_file, 'fasta'):
     if (record.id.isnumeric()) or (record.id in ['X', 'Y', 'MT']):
@@ -10,6 +14,9 @@ for record in SeqIO.parse(ref_file, 'fasta'):
 
 all_chroms = [str(xx) for xx in sorted([int(x) for x in ref.keys() if x.isnumeric()])]
 all_chroms.extend(['X', 'Y', 'MT'])
+
+out_dir = '/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/site_annotations'
+os.makedirs(out_dir, exist_ok=True)
 
 DRACH_motifs = [
     'AAACA',
@@ -32,4 +39,35 @@ DRACH_motifs = [
     'TGACT',
 ]
 
+fields = [
+    'chrom',
+    'chromStart',
+    'chromEnd',
+    'name',
+    'score',
+    'strand',
+    'ref5mer'
+]
+
 for this_chrom in all_chroms:
+    print(f'Parsing chr{this_chrom}')
+    seq_forward = ref[this_chrom]
+    seq_revcomp = str(Seq(ref[this_chrom]).reverse_complement())
+    seq_len = len(seq_forward)
+    chromStart_strand_ref5mer = []
+    for this_motif in tqdm(DRACH_motifs):
+        for this_match in re.finditer(this_motif, seq_forward):
+            chromStart_strand_ref5mer.append((this_match.start()+2, '+', this_motif))
+        for this_match in re.finditer(this_motif, seq_revcomp):
+            chromStart_strand_ref5mer.append((seq_len-this_match.start()-3, '-', this_motif))
+    chromStart_strand_ref5mer.sort()
+
+    this_chrom_df = pd.DataFrame(chromStart_strand_ref5mer, columns=['chromStart', 'strand', 'ref5mer'])
+    this_chrom_df['chrom'] = this_chrom
+    this_chrom_df['chromEnd'] = this_chrom_df['chromStart'] + 1
+    this_chrom_df['name'] = 'DRACH'
+    this_chrom_df['score'] = '.'
+    this_chrom_df = this_chrom_df[fields]
+
+    print(f'Writing out {len(this_chrom_df)} sites')
+    this_chrom_df.to_csv(os.path.join(out_dir, f'DRACH.GRCh38_102.chr{this_chrom}.bed'), sep='\t', index=False, header=True)
--- workflows/test_mAFiA_single.sh
+++ workflows/test_mAFiA_single.sh
@@ -3,19 +3,21 @@
 #SBATCH --exclude=gpu-g4-1
 #SBATCH --gres=gpu:turing:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=500GB
+#SBATCH --mem=160GB
 #SBATCH --verbose
-#SBATCH --job-name=test_mAFiA
-#SBATCH --output=/home/achan/slurm/test_mAFiA_%A.out
+#SBATCH --job-name=DRACH_chrX
+#SBATCH --output=/home/achan/slurm/DRACH_chrX.out
+
+chr=X
 
 workspace=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/HEK293/100_WT_0_IVT
-bam=${workspace}/chrX_q50.bam
-fast5_dir=${workspace}/fast5_chrX
-ref=/biodb/genomes/homo_sapiens/GRCh38_102/GRCh38_102.fa
-mod=/home/achan/Data/GLORI/bed_files/GLORI_chrX_ref5mer.tsv
+bam=${workspace}/chr${chr}/sorted.chr${chr}.bam
+fast5_dir=${workspace}/chr${chr}/fast5
+ref=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/GRCh38_102/GRCh38_102.chr${chr}.fa
+mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/site_annotations/DRACH.GRCh38_102.chr${chr}.bed
 backbone=${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch
 classifiers=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/MAFIA_classifiers/DRACH
-output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/results/mAFiA_deep_chrX
+output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/DRACH/chr${chr}
 
 source ${HOME}/git/mAFiA/mafia-venv/bin/activate
 
