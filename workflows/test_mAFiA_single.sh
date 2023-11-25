249500d70deda7e7a579922cc4072fd31f8adfa0
Adrian <adrian.kaho.chan@gmail.com>
2023-11-25 13:07:06 +0100

    changed paths in workflow

--- workflows/test_mAFiA_single.sh
+++ workflows/test_mAFiA_single.sh
@@ -5,19 +5,20 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=160GB
 #SBATCH --verbose
-#SBATCH --job-name=DRACH_chrX
-#SBATCH --output=/home/achan/slurm/DRACH_chrX.out
+#SBATCH --job-name=METTL3_chr6
+#SBATCH --output=/home/achan/slurm/METTL3_chr6.out
 
-chr=X
+ds=Mettl3-KO
+chr=6
 
-workspace=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/HEK293/100_WT_0_IVT
+workspace=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/HEK293/${ds}
 bam=${workspace}/chr${chr}/sorted.chr${chr}.bam
-fast5_dir=${workspace}/chr${chr}/fast5
+fast5_dir=${workspace}/fast5
 ref=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/GRCh38_102/GRCh38_102.chr${chr}.fa
 mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/site_annotations/DRACH.GRCh38_102.chr${chr}.bed
 backbone=${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch
 classifiers=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/MAFIA_classifiers/DRACH
-output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/DRACH/chr${chr}
+output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/DRACH/${ds}/chr${chr}
 
 source ${HOME}/git/mAFiA/mafia-venv/bin/activate
 
