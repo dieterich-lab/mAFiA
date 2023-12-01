908a9df7705b24d2dd7f8c40835b85b116f8cde9
Adrian <adrian.kaho.chan@gmail.com>
2023-12-01 11:51:21 +0100

    changed db name

--- workflows/prepare_multiple_oligo_datasets.sh
+++ workflows/prepare_multiple_oligo_datasets.sh
@@ -5,7 +5,7 @@
 #DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/oligo/db_WUE_batch3.tsv
 #DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/oligo/db_ISA_mixes1-4.tsv
 #DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/oligo/db_ISA_mixes1-4_m6A.tsv
-DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/oligo/db_ISA_mixes17-19.tsv
+DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/oligo/db_ISA_mixes17-22.tsv
 
 
 {
