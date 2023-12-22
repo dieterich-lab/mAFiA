1bd4d72ccf1e3bd4513b37c9f5017ab703025819
Adrian <adrian.kaho.chan@gmail.com>
2023-12-22 10:15:07 +0100

    changed workflow to mouse

---
+++ workflows/minimap2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#SBATCH --partition=long
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=250GB
+#SBATCH --verbose
+#SBATCH --job-name=minimap_mESC_Mettl3_KO_merged
+#SBATCH --output=/home/achan/slurm/minimap_mESC_Mettl3_KO_merged.out
+
+DATASET=mESC_Mettl3_KO_merged
+#DATASET=mESC_WT_STM_merged
+
+WORKSPACE=/prj/TRR319_RMaP/Project_B01/Adrian/${DATASET}
+REF_GENOME=/biodb/genomes/mus_musculus/GRCm38_102/GRCm38_102.fa
+SAM_GENOME=${WORKSPACE}/genome_mapped.sam
+BAM_GENOME=${WORKSPACE}/genome_filtered_q50.bam
+
+module purge
+module load minimap2
+minimap2 --secondary=no -ax splice -uf -k14 -t 36 --cs ${REF_GENOME} ${WORKSPACE}/basecall_merged.fasta > ${SAM_GENOME}
+
+samtools view -bST ${REF_GENOME} -q50 ${SAM_GENOME} | samtools sort - > ${BAM_GENOME}
+samtools index ${BAM_GENOME}
+
+source ${HOME}/git/mAFiA/mafia-venv/bin/activate
+samtools flagstats ${SAM_GENOME} > genome_qc.txt
+${HOME}/git/renata/accuracy.py ${SAM_GENOME} ${REF_GENOME} >> ${WORKSPACE}/genome_qc.txt
+
+less ${WORKSPACE}/genome_qc.txt
--- workflows/test_mAFiA_single.sh
+++ workflows/test_mAFiA_single.sh
@@ -3,15 +3,21 @@
 #SBATCH --exclude=gpu-g4-1,gpu-g2-1
 #SBATCH --gres=gpu:turing:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=160GB
+#SBATCH --mem=180GB
 #SBATCH --verbose
-#SBATCH --job-name=DRACH_v1_HEK293_WT_chr1
-#SBATCH --output=/home/achan/slurm/DRACH_v1_HEK293_WT_chr1.out
+#SBATCH --job-name=JK_HEK293_STM2457_merged
+#SBATCH --output=/home/achan/slurm/JK_HEK293_STM2457_merged.out
 
 #ds=Mettl3-KO
-ds=100_WT_0_IVT
+#ds=100_WT_0_IVT
+#ds=75_WT_25_IVT
+#ds=50_WT_50_IVT
+#ds=25_WT_75_IVT
 #ds=0_WT_100_IVT
-chr=1
+#chr=X
+
+#ds=JK_HEK293_DMSO_merged
+ds=JK_HEK293_STM2457_merged
 
 #ds=col0
 #ds=vir1
@@ -21,18 +27,24 @@ chr=1
 #ds=IME4_KO
 #chr=IV
 
-workspace=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/HEK293/${ds}
+workspace=/prj/TRR319_RMaP/Project_B01/Adrian/${ds}
+#workspace=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/HEK293/${ds}
 #workspace=/scratch/achan/Arabidopsis_thaliana/${ds}
 #workspace=/scratch/achan/Saccharomyces_cerevisiae/${ds}
-bam=${workspace}/chr${chr}/sorted.chr${chr}.bam
-#bam=${workspace}/filtered_q50.bam
+#bam=${workspace}/chr${chr}/sorted.chr${chr}.bam
+bam=${workspace}/genome_filtered_q50.bam
 
-fast5_dir=${workspace}/chr${chr}/fast5
-#fast5_dir=${workspace}/fast5
+#fast5_dir=${workspace}/chr${chr}/fast5
+fast5_dir=${workspace}/fast5
 
-ref=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/GRCh38_102/GRCh38_102.chr${chr}.fa
+ref=/beegfs/biodb/genomes/homo_sapiens/GRCh38_102/GRCh38_102.fa
+mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/site_annotations/DRACH.GRCh38_102.chrALL.bed
+
+#mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/site_annotations/GLORI_all_ref5mer.bed
+
+#ref=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/GRCh38_102/GRCh38_102.chr${chr}.fa
 #mod=/home/achan/Data/GLORI/bed_files/GLORI_chr${chr}.tsv
-mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/site_annotations/DRACH.GRCh38_102.chr${chr}.bed
+#mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/site_annotations/DRACH.GRCh38_102.chr${chr}.bed
 #ref=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/Arabidopsis_thaliana/reference/TAIR10_chr_all.fasta
 #mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/Arabidopsis_thaliana/reference/6motifs.chr1.bed
 #ref=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/Saccharomyces_cerevisiae/reference/R64-1-1_96.fa
@@ -46,7 +58,9 @@ classifiers=${HOME}/git/mAFiA/models/DRACH_v1
 #output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/DRACH/${ds}/chr${chr}
 #output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/6motifs/Arabidopsis_thaliana/${ds}/chr${chr}
 #output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/6motifs/Saccharomyces_cerevisiae/${ds}/chr${chr}
-output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/DRACH_v1/${ds}/chr${chr}
+#output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/DRACH_v1/${ds}
+
+output=${workspace}/mAFiA
 
 source ${HOME}/git/mAFiA/mafia-venv/bin/activate
 
@@ -56,7 +70,7 @@ test_mAFiA \
 --ref_file ${ref} \
 --mod_file ${mod} \
 --min_coverage 10 \
---max_num_reads 2500 \
+--max_num_reads 1000 \
 --backbone_model_path ${backbone} \
 --classifier_model_dir ${classifiers} \
 --out_dir ${output}
