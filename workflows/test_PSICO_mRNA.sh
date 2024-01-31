32ff6839276bf0ad12f65a868d832b3c74a857d1
Adrian <adrian.kaho.chan@gmail.com>
2024-01-31 15:15:04 +0100

    changed test script

--- workflows/test_PSICO_mRNA.sh
+++ workflows/test_PSICO_mRNA.sh
@@ -9,7 +9,8 @@
 #SBATCH --output=/home/achan/slurm/PSICO_HEK293_%A.out
 
 #ds=Mettl3-KO
-ds="100_WT_0_IVT"
+ds="P2_WT"
+#ds="100_WT_0_IVT"
 #ds=75_WT_25_IVT
 #ds=50_WT_50_IVT
 #ds=25_WT_75_IVT
@@ -24,12 +25,13 @@ bam="${workspace}/genome_filtered_q50.bam"
 fast5_dir="${workspace}/fast5"
 
 ref="/biodb/genomes/homo_sapiens/GRCh38_102/GRCh38_102.fa"
-mod="/prj/TRR319_RMaP/Project_BaseCalling/Adrian/psU/site_annotations/BID_seq.bed"
+#mod="/prj/TRR319_RMaP/Project_BaseCalling/Adrian/psU/site_annotations/BID_seq.bed"
+mod="/prj/TRR319_RMaP/Project_BaseCalling/Adrian/psU/site_annotations/PRAISE.bed"
 
 backbone="${HOME}/git/mAFiA/models/RODAN_HEK293_IVT.torch"
 classifiers="/prj/TRR319_RMaP/Project_BaseCalling/Adrian/psU/PSICO_classifiers/chosen8"
 
-output="/prj/TRR319_RMaP/Project_BaseCalling/Adrian/psU/PSICO_inference/HEK293/${ds}/chosen8"
+output="/prj/TRR319_RMaP/Project_BaseCalling/Adrian/psU/PSICO_inference/HEK293/${ds}/chosen8_PRAISE"
 
 source ${HOME}/git/mAFiA/mafia-venv/bin/activate
 
