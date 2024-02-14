0276a607f20d54d79c5fa36732198d8fa6792d13
Adrian <adrian.kaho.chan@gmail.com>
2024-02-14 14:45:40 +0100

    changed bash script

--- workflows/mAFiA_process_reads.sh
+++ workflows/mAFiA_process_reads.sh
@@ -5,30 +5,32 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=90GB
 #SBATCH --verbose
-#SBATCH --job-name=psico-mAFiA_HEK293_IVT_chr1
-#SBATCH --output=/home/achan/slurm/psico-mAFiA_HEK293_IVT_chr1.out
+#SBATCH --job-name=psico-mAFiA_HEK293_WT_chrX
+#SBATCH --output=/home/achan/slurm/psico-mAFiA_HEK293_WT_chrX.out
 
-#ds=100_WT_0_IVT
-ds=0_WT_100_IVT
-chr=1
+ds=100_WT_0_IVT
+#ds=0_WT_100_IVT
+chr=X
 
 workspace=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/HEK293/${ds}
 bam=${workspace}/chr${chr}/sorted.chr${chr}.bam
 fast5_dir=${workspace}/chr${chr}/fast5
 
-mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/site_annotations/homo_sapiens/GRCh38_102/m6A.psi.GRCh38_102.chr1.bed
+#mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/site_annotations/homo_sapiens/GRCh38_102/m6A.psi.GRCh38_102.chr1.bed
+mod=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/site_annotations/homo_sapiens/GRCh38_102/BID_GLORI.chrX.tsv
 
 backbone=${HOME}/git/mAFiA/models/RODAN_HEK293_IVT.torch
 classifiers=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/psi-co-mAFiA
 
-output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/results/psico-mAFiA_HEK293_${ds}_chr${chr}
+output=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/results/psico-mAFiA/HEK293/${ds}/chr${chr}_parallel
 
 source ${HOME}/git/mAFiA/mafia-venv/bin/activate
 
-python3 ${HOME}/git/mAFiA_dev/mAFiA/mAFiA_process_reads.py \
+python3 ${HOME}/git/mAFiA_dev/mAFiA/mAFiA_process_reads_parallel.py \
 --bam_file ${bam} \
 --fast5_dir ${fast5_dir} \
 --mod_file ${mod} \
 --backbone_model_path ${backbone} \
 --classifier_model_dir ${classifiers} \
+--num_jobs 4 \
 --out_dir ${output}
