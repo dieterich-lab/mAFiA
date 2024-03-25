d828e8c2bb542d69ace6861e5c3fd35e9b12c24a
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-03-25 11:37:38 +0100

    updated psU DB

--- workflows/prepare_multiple_oligo_datasets.sh
+++ workflows/prepare_multiple_oligo_datasets.sh
@@ -7,13 +7,13 @@
 #DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/oligo/db_ISA_mixes1-4_m6A.tsv
 #DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/m6A/oligo/db_ISA_mixes17-22.tsv
 #DB=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/psU/oligo/RNA002/db_PSU_mixes10-11_14-15_23-24.tsv
-DB=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Adrian/Gm/oligo/db_NMG_mixes37-48.tsv
-
+#DB=/beegfs/prj/TRR319_RMaP/Project_BaseCalling/Adrian/Gm/oligo/db_NMG_mixes37-48.tsv
+DB=/beegfs/prj/TRR319_RMaP_BaseCalling/Adrian/psU/oligo/RNA002/db_PSU_mixes51-56.tsv
 {
   read -r
   while read -r o r m h l
   do
     echo "ORIG=$o, RUN=$r, MOD=$m, HOMOPOLYMER=$h, LOC=$l"
-    bash ${HOME}/git/mAFiA_dev/workflows/prepare_single_oligo_dataset_Gm.sh -o "$o" -r "$r" -m "$m" -h "$h" -l "$l" &
+    bash ${HOME}/git/mAFiA_dev/workflows/prepare_single_oligo_dataset_psU.sh -o "$o" -r "$r" -m "$m" -h "$h" -l "$l" &
   done
 } < $DB
