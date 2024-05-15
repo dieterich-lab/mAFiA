7935d4db4ce75f47a12e68a095510cf7b806aed5
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-05-15 10:00:40 +0200

    separate script for merging outputs

---
+++ workflows/merge_mAFiA_outputs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+shopt -s globstar
+
+cd ${WORKSPACE}
+
+########################################################################################################################
+### filter and merge bam ###############################################################################################
+########################################################################################################################
+
+echo "Merging mAFiA outputs at ${WORKSPACE}..."
+
+### filter ###
+#for chr in {1..19} X
+#do
+#  mv chr${chr}/mAFiA.reads.bam chr${chr}/_mAFiA.reads.bam
+#  samtools index chr${chr}/_mAFiA.reads.bam
+#  samtools view -h chr${chr}/_mAFiA.reads.bam ${chr} | samtools sort - > chr${chr}/mAFiA.reads.bam
+#done
+
+## merge bam ###
+samtools merge -o chrALL.mAFiA.reads.bam chr*/mAFiA.reads.bam
+samtools index chrALL.mAFiA.reads.bam
+
+### merge bed ###
+cp chr1/mAFiA.sites.bed chrALL.mAFiA.sites.bed
+for chr in {2..22} X
+do
+  tail -n+2 chr${chr}/mAFiA.sites.bed >> chrALL.mAFiA.sites.bed
+done
