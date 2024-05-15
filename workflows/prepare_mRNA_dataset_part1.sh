56924c24de63d8a79417f82df8fde35ba8b52112
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-05-15 09:33:43 +0200

    split mRNA prep script into 2 parts

---
+++ workflows/prepare_mRNA_dataset_part1.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+shopt -s globstar
+
+MODEL="${HOME}/pytorch_models/HEK293_IVT_2_q50_10M/HEK293_IVT_2_q50_10M-epoch29.torch"
+#REF_GENOME="/biodb/genomes/homo_sapiens/GRCh38_102/GRCh38_102.fa"
+REF_GENOME="/biodb/genomes/mus_musculus/GRCm38_102/GRCm38_102.fa"
+WORKSPACE="/prj/TRR319_RMaP_BaseCalling/Adrian/mouse_heart/Diet/B1_M3KO_CD"
+
+### new HEK293 #########################################################################################################
+#DATASET=0_WT_100_IVT
+#DATASET=25_WT_75_IVT
+#DATASET=50_WT_50_IVT
+#DATASET=75_WT_25_IVT
+#DATASET=100_WT_0_IVT
+#DATASET=P2_WT
+#DATASET=Mettl3-KO
+
+#WORKSPACE=/prj/TRR319_RMaP/Project_BaseCalling/Adrian/HEK293/${DATASET}
+
+########################################################################################################################
+#DATASET=A1_WT_CD
+#DATASET=A2_WT_CD
+#WORKSPACE=/prj/TRR319_RMaP_BaseCalling/Adrian/mouse_heart/Federica_Accornero/${DATASET}
+
+########################################################################################################################
+
+#DATASET=JK_HEK293_DMSO_1_2_RTA
+#DATASET=JK_HEK293_DMSO_3_4_RTA
+#DATASET=JK_HEK293_STM2457_5_6_RTA
+#DATASET=JK_HEK293_STM2457_7_8_RTA
+#DATASET=JK_HEK293_DMSO_merged
+
+#DATASET=mESC_WT_DMSO_merged
+#DATASET=mESC_Mettl3_KO_merged
+#DATASET=mESC_WT_STM_merged
+
+#WORKSPACE=/prj/TRR319_RMaP/Project_B01/Adrian/${DATASET}
+
+########################################################################################################################
+#DATASET=40-26
+#DATASET=40-27
+#DATASET=40-28
+#DATASET=40-29
+#DATASET=40-30
+#DATASET=40-31
+#DATASET=40-32
+#DATASET=40-33
+#DATASET=40-34
+
+#WORKSPACE=/prj/Dewenter_TAC_Backs_lab/achan/${DATASET}
+#DATA_DIR=/prj/Dewenter_TAC_Backs_lab/raw_data/Nanopore_dRNA/Cologne
+########################################################################################################################
+
+#WORKSPACE=/prj/TRR319_RMaP_BaseCalling/Adrian/HeLa_SRR28796313
+
+########################################################################################################################
+#ds=HEK_siCtrl_input_rep1
+#ds=HEK_siMETTL3_input_rep1
+#ds=HEK_siTRUB1_input_rep1
+
+#ds=HEK_siCtrl_input_rep2
+#ds=HEK_siMETTL3_input_rep2
+#ds=HEK_siTRUB1_input_rep2
+
+#WORKSPACE=/prj/TRR319_RMaP_BaseCalling/Adrian/NanoSPA/${ds}
+########################################################################################################################
+
+#mkdir -p ${WORKSPACE}
+cd ${WORKSPACE}
+
+########################################################################################################################
+### basecall large number of reads #####################################################################################
+########################################################################################################################
+source ${HOME}/git/mAFiA/mafia-venv/bin/activate
+
+fast5_dir=${WORKSPACE}/fast5
+FILENAME_PREFIX=fast5_paths_part
+ls -1 ${fast5_dir}/*.fast5 > ${WORKSPACE}/fast5_paths_all
+split -a3 -l10 -d ${WORKSPACE}/fast5_paths_all ${WORKSPACE}/${FILENAME_PREFIX}
+
+echo "Basecalling ${fast5_dir}..."
+
+num_arrays=""
+for f in ${WORKSPACE}/fast5_paths_part*; do ff=${f##*part}; num_arrays+="${ff},"; done
+num_arrays=${num_arrays%,*}
+jid=$(sbatch --parsable --array=${num_arrays} --export=ALL,WORKSPACE=${WORKSPACE},FILENAME_PREFIX=${FILENAME_PREFIX},MODEL=${MODEL} ${HOME}/git/mAFiA_dev/workflows/array_basecaller.sh)
+
+sbatch --dependency=afterok:"${jid}" --export=ALL,WORKSPACE=${WORKSPACE},REF_GENOME=${REF_GENOME} ${HOME}/git/mAFiA_dev/workflows/prepare_mRNA_dataset_part2.sh
---
+++ workflows/prepare_mRNA_dataset_part2.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+shopt -s globstar
+
+cd ${WORKSPACE}
+cat ${WORKSPACE}/part*/rodan.fasta > ${WORKSPACE}/basecall_merged.fasta
+
+########################################################################################################################
+#### align to genome ###################################################################################################
+########################################################################################################################
+sam_genome=${WORKSPACE}/genome_mapped.sam
+bam_genome=${WORKSPACE}/genome_filtered_q50.bam
+
+echo "Mapping to ${REF_GENOME}..."
+
+module purge
+module load minimap2
+srun -c 40 --mem 120GB \
+minimap2 --secondary=no -ax splice -uf -k14 -t 36 --cs ${REF_GENOME} ${WORKSPACE}/basecall_merged.fasta > ${sam_genome}
+
+### check stats and accuracy ###
+echo "Quality control on ${sam_genome}..."
+
+samtools flagstats ${sam_genome} > genome_qc.txt
+echo >> genome_qc.txt
+${HOME}/git/renata/accuracy.py ${sam_genome} ${REF_GENOME} >> genome_qc.txt
+
+#### Convert to BAM and index ###
+samtools view -bST ${REF_GENOME} -q50 ${sam_genome} | samtools sort - > ${bam_genome}
+samtools index ${bam_genome}
+
+#########################################################################################################################
+#### split by chromosome ################################################################################################
+#########################################################################################################################
+echo "Split by chromosome..."
+
+module load ont-fast5-api/4.1.1_deb12
+
+for chr in {1..22} X
+do
+  mkdir -p chr${chr}
+  samtools view -h genome_filtered_q50.bam ${chr} | samtools sort - > chr${chr}/sorted.chr${chr}.bam
+  samtools index chr${chr}/sorted.chr${chr}.bam
+done
+
+for chr in {1..22} X
+do
+  samtools view chr${chr}/sorted.chr${chr}.bam | cut -f1 > chr${chr}/read_ids.txt
+  mkdir -p chr${chr}/fast5
+  srun -c 40 --mem 64GB -o ${HOME}/slurm/fast5_subset_chr${chr}.out -e ${HOME}/slurm/fast5_subset_chr${chr}.err \
+  fast5_subset -t 36 -i fast5 -s chr${chr}/fast5 -l chr${chr}/read_ids.txt &
+done
+
+#########################################################################################################################
+#### clean up ###########################################################################################################
+#########################################################################################################################
+rm -rf ${WORKSPACE}/part*
+rm ${WORKSPACE}/fast5_paths_all
+
+echo "Finished on ${WORKSPACE}"
