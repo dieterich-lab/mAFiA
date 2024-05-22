f79848e1e7ed32a8c78ca2b168f2cf60419c9910
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-05-22 08:58:25 +0200

    small fix

--- workflows/psico_mAFiA_integrated_mouse.sh
+++ workflows/psico_mAFiA_integrated_mouse.sh
@@ -4,4 +4,4 @@ ds="HFpEF/ctrl_H68"
 
 jid=$(sbatch --parsable --export=ALL,ds="${ds}" --array=1-19,23 "${HOME}"/git/mAFiA_dev/workflows/mAFiA_process_reads_mouse.sh)
 
-sbatch --dependency=afterok:"${jid}" --export=ALL,ds=${ds} --array=1-19,23 "${HOME}"/git/mAFiA_dev/workflows/
+sbatch --dependency=afterok:"${jid}" --export=ALL,ds=${ds} --array=1-19,23 "${HOME}"/git/mAFiA_dev/workflows/mAFiA_pileup_mouse.sh
