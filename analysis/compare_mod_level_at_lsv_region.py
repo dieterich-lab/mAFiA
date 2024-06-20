6f679ce3b40173b300f2b8a2396636e12902ad7a
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-06-20 15:51:57 +0200

    new script to plot alt. splicing % by day

--- analysis/compare_mod_level_at_lsv_region.py
+++ analysis/compare_mod_level_at_lsv_region.py
@@ -16,19 +16,18 @@ dict_mod_display = {
 # gtf_file = '/home/adrian/Data/genomes/mus_musculus/GRCm38_102/GRCm38.102.gtf'
 # gtf = BedTool(gtf_file)
 
-
 thresh_conf = 50.0
-day = 'day56'
+# day = 'day1'
 
-majiq_file = f'/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/majiq/TAC/voila_modulize_{day}/summary.tsv'
-sham_file = f'/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/TAC/SHAM_{day}/chrALL.mAFiA.sites.bed'
-tac_file = f'/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/TAC/TAC_{day}/chrALL.mAFiA.sites.bed'
-img_out = f'/home/adrian/img_out/TAC_splice_site_mod_changes/{day}'
+# majiq_file = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/majiq/TAC/voila_modulize_{}/summary.tsv'
+sham_file = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/TAC/SHAM_{}/chrALL.mAFiA.sites.bed'
+tac_file = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/TAC/TAC_{}/chrALL.mAFiA.sites.bed'
+img_out = '/home/adrian/img_out/TAC_splice_site_mod_changes'
 os.makedirs(img_out, exist_ok=True)
 
-df_majiq = pd.read_csv(majiq_file, sep='\t', comment='#')
-df_sham = pd.read_csv(sham_file, sep='\t', dtype={'chrom': str})
-df_tac = pd.read_csv(tac_file, sep='\t', dtype={'chrom': str})
+# df_majiq = pd.read_csv(majiq_file, sep='\t', comment='#')
+# df_sham = pd.read_csv(sham_file, sep='\t', dtype={'chrom': str})
+# df_tac = pd.read_csv(tac_file, sep='\t', dtype={'chrom': str})
 
 def filter_df(in_df, chrom, strand, chromStart, chromEnd, thresh_conf):
     return in_df[
@@ -51,123 +50,136 @@ gene_exon_ranges = {
         # '8': [56787725, 56787836],
         '9': [56787989, 56788170],
         # 'intron': [56788171, 56789143],
-        '10': [56789144, 56789743]
+        # '10': [56789144, 56789743]
     },
     'Synpo2l': {
-        # '3': [20665744, 20666258],
+        '3': [20665744, 20666258],
         # '3intron4': [20664575, 20665743],
-        # '4': [20664397, 20664574],
-        # '4intron5': [20662466, 20664396],
+        '4': [20664397, 20664574],
+        '4intron5': [20662466, 20664396],
         '5': [20658946, 20662465]
     }
 }
 
-for _, row in df_majiq.iterrows():
-    gene_name = row['gene_name']
-    if gene_name not in ['Fhl1', 'Synpo2l']:
-        continue
+# for _, row in df_majiq.iterrows():
+#     gene_name = row['gene_name']
+    # if gene_name not in ['Fhl1', 'Synpo2l']:
+    # if gene_name not in ['Fhl1']:
+    #     continue
 
-    exon_ranges = gene_exon_ranges[gene_name]
-
-    chrom = row['seqid']
-    strand = row['strand']
+    # exon_ranges = gene_exon_ranges[gene_name]
+    # chrom = row['seqid']
+    # strand = row['strand']
     # lsv_regions = row['lsv_id'].split(';')
     # for this_lsv_region in lsv_regions:
     #     source_target = this_lsv_region.split(':')[-2]
     #     chromStart, chromEnd = [int(x) for x in this_lsv_region.split(':')[-1].split('-')]
 
-    num_rows = len(mods)
-    num_cols = len(exon_ranges)
+# gene_name = 'Fhl1'
+# exon_ranges = gene_exon_ranges[gene_name]
+# chrom = 'X'
+# strand = '+'
 
-    plt.figure(figsize=(5 * num_cols, 4 * num_rows))
+gene_name = 'Synpo2l'
+exon_ranges = gene_exon_ranges[gene_name]
+chrom = '14'
+strand = '-'
 
-    for this_col_ind, (this_exon_ind, this_lsv_region) in enumerate(exon_ranges.items()):
-        chromStart, chromEnd = this_lsv_region
-        chromStart -= 1
-        chromEnd -= 1
-
-        sub_df_sham = filter_df(df_sham, chrom, strand, chromStart, chromEnd, thresh_conf)
-        sub_df_tac = filter_df(df_tac, chrom, strand, chromStart, chromEnd, thresh_conf)
+sel_days = ['day1', 'day7', 'day21', 'day56']
+for this_exon_ind, this_exon_range in exon_ranges.items():
+    chromStart, chromEnd = this_exon_range
+    chromStart -= 1
+    chromEnd -= 1
+    df_days = {}
+    for day in sel_days:
+        df_sham = pd.read_csv(sham_file.format(day), sep='\t', dtype={'chrom': str})
+        df_tac = pd.read_csv(tac_file.format(day), sep='\t', dtype={'chrom': str})
+        sub_df_sham = filter_df(df_sham, chrom, strand, chromStart, chromEnd, 0.0)
+        sub_df_tac = filter_df(df_tac, chrom, strand, chromStart, chromEnd, 0.0)
         merged_df_sham_tac = pd.merge(sub_df_sham, sub_df_tac,
                                       on=['chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand', 'ref5mer'],
-                                      how='outer',
-                                      suffixes=['_sham', '_tac']
+                                      suffixes=['_sham', '_tac'],
+                                      how='outer'
                                       )
-        merged_df_sham_tac['delta'] = merged_df_sham_tac['modRatio_tac'] - merged_df_sham_tac['modRatio_sham']
+        merged_df_sham_tac.rename(inplace=True, columns={
+            'modRatio_sham': f'modRatio_sham_{day}',
+            'confidence_sham': f'confidence_sham_{day}',
+            'coverage_sham': f'coverage_sham_{day}',
+            'modRatio_tac': f'modRatio_tac_{day}',
+            'confidence_tac': f'confidence_tac_{day}',
+            'coverage_tac': f'coverage_tac_{day}'
+        })
+        df_days[day] = merged_df_sham_tac
 
-        if len(merged_df_sham_tac)==0:
-            continue
+        num_rows = len(mods)
+        num_cols = 1
+        merged_df_sham_tac[f'delta_{day}'] = merged_df_sham_tac[f'modRatio_tac_{day}'] - merged_df_sham_tac[f'modRatio_sham_{day}']
 
-        for this_row_ind, this_mod in enumerate(mods):
-            plt.subplot(num_rows, num_cols, this_row_ind * num_cols + this_col_ind + 1)
-            this_df = merged_df_sham_tac[merged_df_sham_tac['name']==this_mod]
-            labelled = False
-            for _, this_site in this_df.iterrows():
-                x = this_site['chromEnd']
-                if not labelled:
-                    plt.plot(x, this_site['modRatio_sham'], 'b+', label=f'SHAM_{day}')
-                    plt.plot(x, this_site['modRatio_tac'], 'r+', label=f'TAC_{day}')
-                    labelled = True
+        if len(merged_df_sham_tac)>0:
+            plt.figure(figsize=(5 * num_cols, 4 * num_rows))
+            for this_row_ind, this_mod in enumerate(mods):
+                plt.subplot(num_rows, num_cols, this_row_ind + 1)
+                this_df = merged_df_sham_tac[merged_df_sham_tac['name'] == this_mod]
+                labelled = False
+                for _, this_site in this_df.iterrows():
+                    x = this_site['chromEnd']
+                    if not labelled:
+                        plt.plot(x, this_site[f'modRatio_sham_{day}'], 'b+', label=f'SHAM_{day}')
+                        plt.plot(x, this_site[f'modRatio_tac_{day}'], 'r+', label=f'TAC_{day}')
+                        labelled = True
+                    else:
+                        plt.plot(x, this_site[f'modRatio_sham_{day}'], 'b+')
+                        plt.plot(x, this_site[f'modRatio_tac_{day}'], 'r+')
+                    lc = 'r' if this_site[f'delta_{day}'] >= 0 else 'b'
+                    plt.plot((x, x), (this_site[f'modRatio_sham_{day}'], this_site[f'modRatio_tac_{day}']), '-', c=lc)
+
+                plt.ylim([-5, 100])
+                if this_row_ind == (num_rows - 1):
+                    plt.xlabel('Genome coord.', fontsize=12)
+                plt.ylabel(f'$S_{{{dict_mod_display[this_mod]}}}$', fontsize=12)
+                plt.axvline(chromStart, c='gray')
+                plt.axvline(chromEnd, c='gray')
+                if strand == '+':
+                    plt.xticks([chromStart, chromEnd],
+                               [f"5'\nchr{chrom}: {chromStart + 1}", f"3'\nchr{chrom}: {chromEnd + 1}"])
+                    legend_loc = 'upper left'
                 else:
-                    plt.plot(x, this_site['modRatio_sham'], 'b+')
-                    plt.plot(x, this_site['modRatio_tac'], 'r+')
-                lc = 'r' if this_site['delta']>=0 else 'b'
-                plt.plot((x, x), (this_site['modRatio_sham'], this_site['modRatio_tac']), '-', c=lc)
+                    plt.xticks([chromStart, chromEnd],
+                               [f"3'\nchr{chrom}: {chromStart + 1}", f"5'\nchr{chrom}: {chromEnd + 1}"])
+                    legend_loc = 'upper right'
+                if this_row_ind == 0:
+                    plt.legend(loc=legend_loc)
+                region_len = chromEnd - chromStart
+                plt.xlim([chromStart - 0.1 * region_len, chromEnd + 0.1 * region_len])
+                if this_row_ind == 0:
+                    plt.title(f"exon {this_exon_ind}", fontsize=12)
+            plt.suptitle(f'{gene_name}\n{day}', fontsize=15)
 
-            # for i, this_tx_id in enumerate(unique_tx_ids):
-            #     tx_exons = [this_exon for this_exon in exons if this_exon.attrs['transcript_id']==this_tx_id]
-            #     for this_exon in tx_exons:
-            #     # if this_exon.attrs['exon_id'] not in used_exon_ids:
-            #         plt.axvline(this_exon.start, c=tx_colors[i])
-            #         plt.axvline(this_exon.end, c=tx_colors[i])
-            #         # plt.text(0.5*(this_exon.start+this_exon.end), 100, this_exon.attrs['exon_number'])
-            #     # used_exon_ids.append(this_exon.attrs['exon_id'])
+            plt.savefig(os.path.join(img_out, f'{gene_name}_{day}.png'), bbox_inches='tight')
+            plt.close('all')
 
-            plt.ylim([-5, 100])
-            if this_row_ind == (num_rows-1):
-                plt.xlabel('Genome coord.', fontsize=12)
-            plt.ylabel(f'$S_{{{dict_mod_display[this_mod]}}}$', fontsize=12)
-            plt.axvline(chromStart, c='gray')
-            plt.axvline(chromEnd, c='gray')
-            if strand == '+':
-                plt.xticks([chromStart, chromEnd], [f"5'\nchr{chrom}: {chromStart+1}", f"3'\nchr{chrom}: {chromEnd+1}"])
-                legend_loc = 'upper left'
-            else:
-                plt.xticks([chromStart, chromEnd], [f"3'\nchr{chrom}: {chromStart+1}", f"5'\nchr{chrom}: {chromEnd+1}"])
-                legend_loc = 'upper right'
-            if this_row_ind == 0 and this_col_ind == 0:
-                plt.legend(loc=legend_loc)
-            region_len = chromEnd - chromStart
-            plt.xlim([chromStart-0.1*region_len, chromEnd+0.1*region_len])
-            if this_row_ind==0:
-                plt.title(f"exon {this_exon_ind}", fontsize=12)
-    plt.suptitle(f'{gene_name}\n{day}', fontsize=15)
+    df_all_days = pd.merge(df_days['day1'], df_days['day7'],
+                           on=['chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand', 'ref5mer'],
+                           )
+    df_all_days = pd.merge(df_all_days, df_days['day21'],
+                           on=['chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand', 'ref5mer'],
+                           )
+    df_all_days = pd.merge(df_all_days, df_days['day56'],
+                           on=['chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand', 'ref5mer'],
+                           )
 
-    plt.savefig(os.path.join(img_out, f'{gene_name}_{day}.png'), bbox_inches='tight')
-    plt.close('all')
-
-        # plt.figure(figsize=(10, 5))
-        # for this_subplot, this_mod in enumerate(['m6A', 'psi']):
-        #     plt.subplot(1, 2, this_subplot+1)
-        #     this_df = merged_df_sham_tac[merged_df_sham_tac['name']==this_mod]
-        #     plt.plot(this_df['delta'], this_df['modRatio_sham'], '.')
-        #     plt.xlim([-20, 20])
-        #     plt.ylim([-5, 100])
-        #     plt.xlabel('${\Delta}S$')
-        #     plt.ylabel('$S_{SHAM}$')
-        #     plt.title(this_mod)
-        # plt.suptitle(f"{this_lsv_region}\n{row['module_event_combination']}", fontsize=15)
-
-        # df_bed = pd.DataFrame.from_dict({
-        #     'chrom': [chrom],
-        #     'chromStart': [chromStart],
-        #     'chromEnd': [chromEnd],
-        #     'name': [this_lsv_region],
-        #     'score': ['.'],
-        #     'strand': [strand]
-        # })
-        # annotations = gtf.intersect(BedTool.from_dataframe(df_bed))
-        # exons = [annot for annot in annotations if
-        #         ('exon_number' in annot.attrs.keys()) and ('exon_id' in annot.attrs.keys())]
-        # unique_tx_ids = list(set([this_exon.attrs['transcript_id'] for this_exon in exons]))
-        # tx_colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_tx_ids)))
+    for _, this_row in df_all_days.iterrows():
+        plt.figure(figsize=(5, 4))
+        plt.plot(range(1, len(sel_days)+1), [this_row['modRatio_sham_day1'], this_row['modRatio_sham_day7'], this_row['modRatio_sham_day21'], this_row['modRatio_sham_day56']], 'b-o', label='SHAM')
+        plt.plot(range(1, len(sel_days)+1), [this_row['modRatio_tac_day1'], this_row['modRatio_tac_day7'], this_row['modRatio_tac_day21'], this_row['modRatio_tac_day56']], 'r-o', label='TAC')
+        plt.legend(loc='upper left')
+        plt.xticks(range(1, len(sel_days)+1), sel_days)
+        plt.xlabel('Day', fontsize=12)
+        plt.ylabel('S', fontsize=12)
+        plt.title(f"chr{this_row['chrom']}: {this_row['chromEnd']}, ${{{dict_mod_display[this_row['name']]}}}$", fontsize=15)
+        plt.savefig(
+            os.path.join(img_out,
+                         f"{gene_name}_chr{this_row['chrom']}_{this_row['chromEnd']}_{this_row['name']}.png"),
+            bbox_inches='tight'
+        )
+        plt.close('all')
---
+++ analysis/plot_AS_percentage_by_day.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+width = 0.4
+
+### Fhl1 ###
+# gene_name = 'Fhl1'
+# events = ['3 -> 9', '5 -> 9']
+# colors = ['red', 'purple']
+# counts = {
+#     'TAC': {
+#         'day1': [119, 25],
+#         'day7': [158, 593],
+#         'day21': [168, 820],
+#         'day56': [189, 865]
+#     },
+#     'SHAM': {
+#         'day1': [109, 14],
+#         'day7': [147, 15],
+#         'day21': [172, 19],
+#         'day56': [153, 63]
+#     }
+# }
+
+### Synpo2l ###
+gene_name = 'Synpo2l'
+events = ['3 -> 5', 'i -> 5']
+colors = ['red', 'green']
+counts = {
+    'TAC': {
+        'day1': [198, 11],
+        'day7': [166, 135],
+        'day21': [227, 468],
+        'day56': [197, 471]
+    },
+    'SHAM': {
+        'day1': [161, 10],
+        'day7': [119, 19],
+        'day21': [170, 10],
+        'day56': [117, 47]
+    }
+}
+
+event_colors = {ev: c for ev, c in zip(events, colors)}
+
+img_out = '/home/adrian/img_out/TAC_splice_site_mod_changes'
+os.makedirs(img_out, exist_ok=True)
+
+days = [
+    'day1',
+    'day7',
+    'day21',
+    'day56'
+]
+
+
+def normalize_and_recast(in_counts, in_events):
+    norm_counts = np.array([[this_count[0]/sum(this_count), this_count[1]/sum(this_count)] for this_count in in_counts.values()])
+    return {in_events[i]: norm_counts[:, i] for i in range(len(events))}
+
+
+fig = plt.figure(figsize=(8, 8))
+for subplot_ind, ds in enumerate(['SHAM', 'TAC']):
+    ds_ratios = normalize_and_recast(counts[ds], events)
+    ax = fig.add_subplot(2, 1, subplot_ind+1)
+    bottom = np.zeros(len(days))
+    for this_event, this_ratio in ds_ratios.items():
+        p = ax.bar(days, this_ratio, width, label=this_event, bottom=bottom, color=event_colors[this_event])
+        bottom += this_ratio
+    plt.ylabel('Splice Variant Ratio (%)', fontsize=12)
+    plt.ylim([0, 1.1])
+    plt.yticks(np.arange(0, 1.1, 0.25), np.int32(np.arange(0, 1.1, 0.25)*100))
+    plt.legend(loc='upper right', title='Splice variant')
+    plt.title(ds, fontsize=12)
+plt.xlabel('Days', fontsize=12)
+plt.suptitle(gene_name, fontsize=15)
+fig.savefig(os.path.join(img_out, f'{gene_name}_AS_progression.png'), bbox_inches='tight')
