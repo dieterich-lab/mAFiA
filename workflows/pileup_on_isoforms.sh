a2dee751a66f491ce646a89b0895ef6a5753c1ba
Adrian <adrian.kaho.chan@gmail.com>
2024-07-02 16:52:04 +0200

    update pileup workflow

--- workflows/pileup_on_isoforms.sh
+++ workflows/pileup_on_isoforms.sh
@@ -3,38 +3,31 @@
 #SBATCH --cpus-per-task=40
 #SBATCH --mem=90GB
 #SBATCH --verbose
-#SBATCH --job-name=pileup_Rcan1
-#SBATCH --output=/home/achan/slurm/pileup_Rcan1_%A.out
+#SBATCH --job-name=pileup_Fhl1
+#SBATCH --output=/home/achan/slurm/pileup_Fhl1_%A.out
 
-workspace="/beegfs/prj/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/TAC/TAC_day1"
+workspace="/beegfs/prj/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/TAC/${ds}/bambu"
 out_dir=${workspace}
 
+gene="Fhl1"
+chr="X"
+isoforms="ENSMUST00000023854 ENSMUST00000114772 unclassified.new_exon"
+
 cd ${workspace}
-
-chr="16"
-
-samtools view -f 16 chrALL.mAFiA.reads.bam 16:92399896-92400077 -o Rcan1.mAFiA.reads.bam
-samtools index Rcan1.mAFiA.reads.bam
-
-samtools view Rcan1.mAFiA.reads.bam 16:92465833-92466146 -o Rcan1.exon2.mAFiA.reads.bam
-samtools index Rcan1.exon2.mAFiA.reads.bam
-
-samtools view Rcan1.exon2.mAFiA.reads.bam | cut -f1 > ids_exon2
-samtools view -N ids_exon2 Rcan1.mAFiA.reads.bam -U Rcan1.exon4.mAFiA.reads.bam
-samtools index Rcan1.exon4.mAFiA.reads.bam
 
 source ${HOME}/git/mAFiA/mafia-venv/bin/activate
 mod=/prj/TRR319_RMaP_BaseCalling/Adrian/site_annotations/mus_musculus/GRCm38_102/m6A.psi.GRCm38_102.chr${chr}.bed
 
-for exon in 2 4
+for isoform in ${isoforms}
 do
-  bam="${workspace}/Rcan1.exon${exon}.mAFiA.reads.bam"
+  bam="${workspace}/${gene}.${isoform}.bam"
+  samtools index ${bam}
   python3 ${HOME}/git/mAFiA_dev/mAFiA/mAFiA_pileup.py \
     --bam_file ${bam} \
     --mod_file ${mod} \
     --min_coverage 1 \
     --out_dir ${out_dir} \
-    --out_filename "Rcan1.exon${exon}.bed" \
+    --out_filename "${gene}.${isoform}.bed" \
     --num_jobs 36
 done
 
