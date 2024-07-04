cebb5121b861f97b1c49b96e8bd844b7a2b1792e
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-07-04 21:01:56 +0200

    legend in plot

--- analysis/plot_single_site_between_isoforms.py
+++ analysis/plot_single_site_between_isoforms.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 chrom = 'X'
 # chromEnd = 56792649
-# chromEnd = 56791998
+chromEnd = 56791998
 days = ['day1', 'day7', 'day21']
 isoforms = ['ENSMUST00000023854', 'ENSMUST00000114772']
 colors = ['red', 'purple']
@@ -29,6 +29,7 @@ for this_isoform in isoforms:
         mod = df_sel['name'].values[0]
     plt.plot(day_values, '-o', label=this_isoform, c=iso_colors[this_isoform])
     # plt.plot(day_values, 'o', label=this_isoform, c=iso_colors[this_isoform])
+plt.legend(loc='upper left', fontsize=10)
 plt.ylim([60, 100])
 plt.ylabel(f'$S_{{{dict_mod_display[mod]}}}$', fontsize=12)
 plt.xticks(range(len(days)), days)
