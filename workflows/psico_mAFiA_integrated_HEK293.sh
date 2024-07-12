12992cbdab36799ff0bcc2a469714cd14c838f3c
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-07-12 10:13:24 +0200

    changed ds in integrated script

--- workflows/psico_mAFiA_integrated_HEK293.sh
+++ workflows/psico_mAFiA_integrated_HEK293.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ds="WT_P2"
+ds="HEK293_TRUB1_OE/rep1"
 
 jid=$(sbatch --parsable --export=ALL,ds="${ds}" --array=1-23 "${HOME}"/git/mAFiA_dev/workflows/mAFiA_process_reads_HEK293.sh)
 
