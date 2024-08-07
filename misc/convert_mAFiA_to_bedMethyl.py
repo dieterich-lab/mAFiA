59c13fbfaed68136409a3322d2e6abc8540c347d
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-08-07 14:46:12 +0200

    fixed ont flag

--- misc/convert_mAFiA_to_bedMethyl.py
+++ misc/convert_mAFiA_to_bedMethyl.py
@@ -3,6 +3,7 @@ import pandas as pd
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 import numpy as np
+import os
 
 # infile = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/HEK293/WT_P2/chrALL.mAFiA.sites.bed'
 # outfile = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/HEK293/WT_P2/chrALL.bedMethyl'
@@ -10,7 +11,7 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--infile')
 parser.add_argument('-o', '--outfile')
-parser.add_argument('--ont', action='store_true')
+parser.add_argument('--modkit', action='store_true')
 args = parser.parse_args()
 print(args)
 
@@ -41,7 +42,7 @@ bedMethyl_fields = [
     'frequency'
 ]
 
-if args.ont:
+if args.modkit:
     dict_mod_names = {
         'm6A': 'a',
         'psi': '17802'
@@ -62,12 +63,12 @@ out_bed['score'] = np.int64(np.round(log_pval))
 out_bed['thickStart'] = out_bed['chromStart']
 out_bed['thickEnd'] = out_bed['chromEnd']
 out_bed['itemRgb'] = '0,0,0'
-if args.ont:
+if args.modkit:
     out_bed['frequency'] = np.round(in_bed['modRatio'], 2)
 else:
     out_bed['frequency'] = np.int64(np.round(in_bed['modRatio']))
 
 out_bed = out_bed[bedMethyl_fields]
-if args.ont:
-    outfile = "modkit." + args.outfile
+if args.modkit:
+    outfile = os.path.join(os.path.dirname(args.outfile), 'modkit.' + os.path.basename(args.outfile))
 out_bed.to_csv(outfile, sep='\t', index=False)
