026d2cb6612fbd5c05ff0c56bbc764f80ad72ef0
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-08-20 12:01:46 +0200

    generalize metaplot python to all ds

--- analysis/compare_metaplot_distance_measures.py
+++ analysis/compare_metaplot_distance_measures.py
@@ -5,10 +5,17 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-ds = 'HFpEF'
-conditions = ['ctrl_merged', 'HFpEF_merged']
+# ds = 'HFpEF'
+# conditions = ['ctrl_merged', 'HFpEF_merged']
+
+# ds = 'Diet'
+# conditions = ['WT_CD_merged', 'WT_WD_merged']
+
+ds = 'TAC'
+conditions = ['SHAM_merged', 'TAC_merged']
+
 cond_names = {x: x.rstrip('_merged') for x in conditions}
-cond_colors = {'ctrl_merged': 'blue', 'HFpEF_merged': 'red'}
+cond_colors = {this_cond: this_color for this_cond, this_color in zip(conditions, ['blue', 'red'])}
 
 mods = ['m6A', 'psi']
 dict_mod_display = {
@@ -21,18 +28,18 @@ thresholds = ['0.0', '50.0']
 metaplot_dir = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/metaPlotR'
 img_out = '/home/adrian/img_out/metatranscript'
 
-coverage_dir = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/HFpEF'
-cond_coverage_files = {
-    'ctrl_merged': os.path.join(coverage_dir, 'ctrl_merged.geneBodyCoverage.txt'),
-    'HFpEF_merged': os.path.join(coverage_dir, 'ctrl_merged.geneBodyCoverage.txt'),
-}
+# coverage_dir = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/HFpEF'
+# cond_coverage_files = {
+#     'ctrl_merged': os.path.join(coverage_dir, 'ctrl_merged.geneBodyCoverage.txt'),
+#     'HFpEF_merged': os.path.join(coverage_dir, 'ctrl_merged.geneBodyCoverage.txt'),
+# }
 
-cond_coverage = {}
-for this_cond in conditions:
-    this_cond_df_coverage = pd.read_csv(cond_coverage_files[this_cond], sep='\t', header=None).T
-    this_cond_df_coverage = this_cond_df_coverage[1:]
-    this_cond_df_coverage = this_cond_df_coverage.rename(columns={0: 'percentile', 1: 'coverage'})
-    cond_coverage[this_cond] = np.float64(this_cond_df_coverage['coverage'].values)
+# cond_coverage = {}
+# for this_cond in conditions:
+#     this_cond_df_coverage = pd.read_csv(cond_coverage_files[this_cond], sep='\t', header=None).T
+#     this_cond_df_coverage = this_cond_df_coverage[1:]
+#     this_cond_df_coverage = this_cond_df_coverage.rename(columns={0: 'percentile', 1: 'coverage'})
+#     cond_coverage[this_cond] = np.float64(this_cond_df_coverage['coverage'].values)
 
 cond_mod_thresh_dist_measure = {
     this_cond: {
@@ -50,7 +57,7 @@ bin_edges = np.linspace(*hist_range, hist_num_bins+1)
 bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
 
 
-def smooth(y, box_pts):
+def smooth(y, box_pts=5):
     box = np.ones(box_pts)/box_pts
     y_smooth = np.convolve(y, box, mode='same')
     return y_smooth
@@ -68,11 +75,11 @@ for mod_ind, this_mod in enumerate(mods):
         )
         # norm_hist = this_cond_mod_hist / cond_coverage[this_cond]
         norm_hist = this_cond_mod_hist / this_cond_mod_hist_all
-        norm_hist_smoothed = smooth(norm_hist, 5)
+        norm_hist_smoothed = smooth(norm_hist)
         plt.plot(bin_centers, norm_hist_smoothed, c=cond_colors[this_cond], label=cond_names[this_cond])
     plt.legend()
-    plt.axvline(x=1, c='gray', ls='--')
-    plt.axvline(x=2, c='gray', ls='--')
+    plt.axvline(x=1, c='gray', alpha=0.5)
+    plt.axvline(x=2, c='gray', alpha=0.5)
     plt.xticks([0.5, 1.5, 2.5], ['5\' UTR', 'CDS', '3\' UTR'])
     plt.ylabel('$N_{S\geq50}$ / $N_{covered}$', fontsize=12)
     plt.ylim([0, 0.1])
--- analysis/export_mAFiA_bed_for_metaplotR.py
+++ analysis/export_mAFiA_bed_for_metaplotR.py
@@ -10,27 +10,38 @@ bed6_fields = [
     'strand'
 ]
 
-thresh_conf = 50.0
-thresh_modRatio = 50.0
+# thresh_conf = 50.0
+# thresh_modRatio = 50.0
 
-ds = 'HFpEF'
+# thresh_conf = 0.0
+# thresh_modRatio = 0.0
+
+# ds = 'HFpEF'
+# conditions = ['ctrl_merged', 'HFpEF_merged']
+
+# ds = 'Diet'
+# conditions = ['WT_CD_merged', 'WT_WD_merged']
+
+ds = 'TAC'
+conditions = ['SHAM_merged', 'TAC_merged']
+
 res_dir = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart'
 
-for this_cond in ['ctrl_merged', 'HFpEF_merged']:
+for (thresh_conf, thresh_modRatio) in [(0.0, 0.0), (50.0, 50.0)]:
+    for this_cond in conditions:
+        in_bed_file = os.path.join(res_dir, ds, this_cond, 'chrALL.mAFiA.sites.bed')
 
-    in_bed_file = os.path.join(res_dir, ds, this_cond, 'chrALL.mAFiA.sites.bed')
+        df_in = pd.read_csv(in_bed_file, sep='\t', dtype={'chrom': str})
+        df_in_thresh = df_in[
+            (df_in['modRatio'] >= thresh_modRatio)
+            * (df_in['confidence'] >= thresh_conf)
+            ]
 
-    df_in = pd.read_csv(in_bed_file, sep='\t', dtype={'chrom': str})
-    df_in_thresh = df_in[
-        (df_in['modRatio'] >= thresh_modRatio)
-        * (df_in['confidence'] >= thresh_conf)
-        ]
+        for this_mod in ['m6A', 'psi']:
+            out_bed_file = os.path.join(res_dir, 'metaPlotR', f'{ds}_{this_cond}_{this_mod}_modRatio{thresh_modRatio}.bed')
 
-    for this_mod in ['m6A', 'psi']:
-        out_bed_file = os.path.join(res_dir, 'metaPlotR', f'{ds}_{this_cond}_{this_mod}_modRatio{thresh_modRatio}.bed')
+            df_out = df_in_thresh[df_in_thresh['name'] == this_mod]
+            df_out['chrom'] = ['chr' + this_chr for this_chr in df_out['chrom']]
+            df_out['score'] = df_out['modRatio']
 
-        df_out = df_in_thresh[df_in_thresh['name'] == this_mod]
-        df_out['chrom'] = ['chr' + this_chr for this_chr in df_out['chrom']]
-        df_out['score'] = df_out['modRatio']
-
-        df_out[bed6_fields].to_csv(out_bed_file, sep='\t', header=False, index=False)
+            df_out[bed6_fields].to_csv(out_bed_file, sep='\t', header=False, index=False)
