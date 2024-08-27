6f0255ff516a345104689d6f1829c08343b69da9
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-08-27 15:37:18 +0200

    update

--- analysis/calc_avg_mod_in_IR_reads.py
+++ analysis/calc_avg_mod_in_IR_reads.py
@@ -9,6 +9,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import pybedtools
 
 
 mods = ['m6A', 'psi']
@@ -20,6 +21,12 @@ dict_mod_code = {
     'm6A': 21891,
     'psi': 17802,
 }
+mod_colors = {
+    'm6A': 'red',
+    'psi': 'blue'
+}
+mod_labels = [(mpatches.Patch(color=this_color), this_label)
+              for this_label, this_color in mod_colors.items()]
 
 def calc_mod_level_per_read(in_reads, in_num_bases=5):
     out_read_avg_mod_level = {}
@@ -36,7 +43,8 @@ def calc_mod_level_per_read(in_reads, in_num_bases=5):
 
 # roi_bin_edges = [-100, -50, 0, 50, 100]
 roi_max_bin = 150
-roi_bin_edges = np.arange(-roi_max_bin, roi_max_bin+1, 50)
+roi_bin_width = 50
+roi_bin_edges = np.arange(-roi_max_bin, roi_max_bin+1, roi_bin_width)
 x_zero_ind = np.where(np.array(roi_bin_edges) == 0)[0][0]
 x_zero = x_zero_ind - 0.5
 def get_binned_modProbs_near_roi(in_refPos_modProbs, roi_coord):
@@ -61,15 +69,33 @@ def aggregate_modProb_bins(in_modProbs):
     return out_modProbs
 
 
+def calc_log2fc(mod_probs_1, mod_probs_2):
+    out_log2fc = []
+    for this_bin_1, this_bin_2 in zip(mod_probs_1, mod_probs_2):
+        if (len(this_bin_1) == 0) or (len(this_bin_2) == 0):
+            out_log2fc.append([])
+        else:
+            n1 = np.sum(np.array(this_bin_1) >= 0.5)
+            n2 = np.sum(np.array(this_bin_2) >= 0.5)
+            if (n1 == 0) or (n2 == 0):
+                out_log2fc.append([])
+            else:
+                out_log2fc.append([np.log2(n1/n2)])
+    return out_log2fc
+
+
 thresh_IntronDepth = 10
-thresh_IRratio = 0.05
-thresh_intronic_percentage = 0.05
+thresh_IRratio = 0.1
+thresh_intronic_percentage = 0.1
 
 ds = 'HFpEF'
 cond = 'ctrl_merged'
 res_dir = f'/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/{ds}/{cond}'
 irf_file = os.path.join(res_dir, 'IRFinder/IRFinder-IR-nondir.txt')
 bam_file = os.path.join(res_dir, 'chrALL.mAFiA.reads.bam')
+
+gtf_file = '/home/adrian/Data/genomes/mus_musculus/GRCm38_102/GRCm38.102.gtf'
+gtf_bedtools = pybedtools.BedTool(gtf_file)
 
 img_out = '/home/adrian/img_out/IR_reads_avg_mod'
 os.makedirs(img_out, exist_ok=True)
@@ -78,20 +104,19 @@ df_irf = pd.read_csv(irf_file, sep='\t', dtype={'Chr': str})
 df_irf_thresh = df_irf[
     (df_irf['IntronDepth'] >= thresh_IntronDepth)
     * (df_irf['IRratio'] >= thresh_IRratio)
+    * (df_irf['IRratio'] < 1.0)
 ]
-df_irf_clean = df_irf_thresh
-# df_irf_clean = df_irf_thresh.loc[[this_name.split('/')[-1] == 'clean' for this_name in df_irf_thresh['Name']], :]
-# df_irf_clean = df_irf_clean[df_irf_clean['Warnings'] != 'MinorIsoform']
+# df_irf_clean = df_irf_thresh
+df_irf_clean = df_irf_thresh.loc[[this_name.split('/')[-1] == 'clean' for this_name in df_irf_thresh['Name']], :]
+# df_irf_clean = df_irf_clean[df_irf_clean['Warnings'] == '-']
 
 intronic_reads = []
 exonic_reads = []
-# mean_intronic_roiStart_modProbs = {this_mod: [] for this_mod in mods}
-# mean_intronic_roiEnd_modProbs = {this_mod: [] for this_mod in mods}
-# mean_exonic_roiStart_modProbs = {this_mod: [] for this_mod in mods}
-# mean_exonic_roiEnd_modProbs = {this_mod: [] for this_mod in mods}
 mean_delta_roiStart_modProbs = {this_mod: [] for this_mod in mods}
 mean_delta_roiEnd_modProbs = {this_mod: [] for this_mod in mods}
 for _, this_row in tqdm(df_irf_clean.iterrows()):
+# for this_name in tqdm(df_irf_clean['Name'].unique()):
+#     this_row = df_irf_clean[df_irf_clean['Name'] == this_name].iloc[0]
     intronic_roiStart_modProbs = {this_mod: [] for this_mod in mods}
     intronic_roiEnd_modProbs = {this_mod: [] for this_mod in mods}
     exonic_roiStart_modProbs = {this_mod: [] for this_mod in mods}
@@ -105,6 +130,9 @@ for _, this_row in tqdm(df_irf_clean.iterrows()):
         else:
             roiStart = irEnd
             roiEnd = irStart
+
+        # this_row_bed = pybedtools.BedTool(f'{chrom} {irStart} {irEnd}', from_string=True)
+        # intersect = gtf_bedtools.intersect(this_row_bed, wa=True)
 
         for this_read in bam.fetch(chrom, irStart, irEnd):
             if this_read.flag == flag_required:
@@ -139,98 +167,30 @@ for _, this_row in tqdm(df_irf_clean.iterrows()):
     exonic_roiEnd_modProbs = aggregate_modProb_bins(exonic_roiEnd_modProbs)
 
     for this_mod in mods:
-        mean_delta_roiStart_modProbs[this_mod].append([[np.log2(np.mean(this_intronic_bin)/np.mean(this_exonic_bin))]
-                                                       if (len(this_intronic_bin) and len(this_exonic_bin)) else []
-                                                       for this_intronic_bin, this_exonic_bin
-                                                       in zip(intronic_roiStart_modProbs[this_mod],
-                                                              exonic_roiStart_modProbs[this_mod])])
-        mean_delta_roiEnd_modProbs[this_mod].append([[np.log2(np.mean(this_intronic_bin)/np.mean(this_exonic_bin))]
-                                                       if (len(this_intronic_bin) and len(this_exonic_bin)) else []
-                                                       for this_intronic_bin, this_exonic_bin
-                                                       in zip(intronic_roiEnd_modProbs[this_mod],
-                                                              exonic_roiEnd_modProbs[this_mod])])
-
-        # mean_intronic_roiStart_modProbs[this_mod].append([[np.mean(this_bin)] if len(this_bin) else []
-        #                                                  for this_bin in intronic_roiStart_modProbs[this_mod]])
-        # mean_intronic_roiEnd_modProbs[this_mod].append([[np.mean(this_bin)] if len(this_bin) else []
-        #                                                for this_bin in intronic_roiEnd_modProbs[this_mod]])
-        # mean_exonic_roiStart_modProbs[this_mod].append([[np.mean(this_bin)] if len(this_bin) else []
-        #                                                for this_bin in exonic_roiStart_modProbs[this_mod]])
-        # mean_exonic_roiEnd_modProbs[this_mod].append([[np.mean(this_bin)] if len(this_bin) else []
-        #                                               for this_bin in exonic_roiEnd_modProbs[this_mod]])
-        # mean_intronic_roiStart_modProbs[this_mod].append(intronic_roiStart_modProbs[this_mod])
-        # mean_intronic_roiEnd_modProbs[this_mod].append(intronic_roiEnd_modProbs[this_mod])
-        # mean_exonic_roiStart_modProbs[this_mod].append(exonic_roiStart_modProbs[this_mod])
-        # mean_exonic_roiEnd_modProbs[this_mod].append(exonic_roiEnd_modProbs[this_mod])
-
+        mean_delta_roiStart_modProbs[this_mod].append(
+            calc_log2fc(intronic_roiStart_modProbs[this_mod], exonic_roiStart_modProbs[this_mod])
+        )
+        mean_delta_roiEnd_modProbs[this_mod].append(
+            calc_log2fc(intronic_roiEnd_modProbs[this_mod], exonic_roiEnd_modProbs[this_mod])
+        )
 
 mean_delta_roiStart_modProbs = aggregate_modProb_bins(mean_delta_roiStart_modProbs)
 mean_delta_roiEnd_modProbs = aggregate_modProb_bins(mean_delta_roiEnd_modProbs)
 
-# mean_intronic_roiStart_modProbs = aggregate_modProb_bins(mean_intronic_roiStart_modProbs)
-# mean_intronic_roiEnd_modProbs = aggregate_modProb_bins(mean_intronic_roiEnd_modProbs)
-# mean_exonic_roiStart_modProbs = aggregate_modProb_bins(mean_exonic_roiStart_modProbs)
-# mean_exonic_roiEnd_modProbs = aggregate_modProb_bins(mean_exonic_roiEnd_modProbs)
-
-# roiStart_modProbs = {
-#     'intronic': {
-#         'start': mean_intronic_roiStart_modProbs,
-#         'end': mean_intronic_roiEnd_modProbs
-#     },
-#     'exonic': {
-#         'start': mean_exonic_roiStart_modProbs,
-#         'end': mean_exonic_roiEnd_modProbs
-#     }
-# }
 mean_delta_modProbs = {
     'start': mean_delta_roiStart_modProbs,
     'end': mean_delta_roiEnd_modProbs
 }
 
-
-# plt.figure(figsize=(10, 10))
-# for row_ind, this_mod in enumerate(mods):
-#     for col_ind, roi in enumerate(['start', 'end']):
-#         plt.subplot(2, 2, row_ind*2+col_ind+1)
-#         labels = [(mpatches.Patch(color=this_color), this_label)
-#                   for this_color, this_label in zip(['b', 'r'], ['non-intronic', 'intronic'])]
-#         for mini_col_ind, (this_intronic_bin, this_exonic_bin) \
-#                 in enumerate(zip(mean_intronic_roiStart_modProbs[this_mod], mean_exonic_roiStart_modProbs[this_mod])):
-#             bplot = plt.boxplot(this_exonic_bin, positions=[mini_col_ind-0.2], showfliers=False, patch_artist=True)
-#             for patch in bplot['boxes']:
-#                 patch.set_facecolor('b')
-#             for patch in bplot['medians']:
-#                 patch.set_color('k')
-#             bplot = plt.boxplot(this_intronic_bin, positions=[mini_col_ind+0.2], showfliers=False, patch_artist=True)
-#             for patch in bplot['boxes']:
-#                 patch.set_facecolor('r')
-#             for patch in bplot['medians']:
-#                 patch.set_color('k')
-#         if roi == 'start':
-#             plt.xlim([-0.5, x_zero])
-#             plt.xticks(np.arange(x_zero + 1) - 0.5, roi_bin_edges[:(x_zero_ind + 1)])
-#         elif roi == 'end':
-#             plt.xlim([x_zero, len(roi_bin_edges)-1.5])
-#             plt.xticks(np.arange(x_zero, len(roi_bin_edges)-1), roi_bin_edges[x_zero_ind:])
-#         plt.ylim([0, 1])
-#         plt.legend(*zip(*labels), loc='upper left')
-#         plt.title(f'${{{dict_mod_display[this_mod]}}}$', fontsize=15)
-#         plt.xlabel(f'Distance from IR {roi}', fontsize=12)
-#         plt.ylabel('Mean mod. prob', fontsize=12)
-
-mod_colors = {
-    'm6A': 'red',
-    'psi': 'blue'
-}
-labels = [(mpatches.Patch(color=this_color), this_label)
-          for this_label, this_color in mod_colors.items()]
+ylim = [-10, 10]
 plt.figure(figsize=(10, 4))
 for col_ind, roi in enumerate(['start', 'end']):
     plt.subplot(1, 2, col_ind+1)
     for mod_ind, this_mod in enumerate(mods):
         for mini_col_ind, this_delta in enumerate(mean_delta_roiStart_modProbs[this_mod]):
+            this_delta = [x for x in this_delta if ~np.isinf(x)]
             bplot = plt.boxplot(this_delta, positions=[mini_col_ind-0.2+0.4*mod_ind],
-                                showfliers=False, patch_artist=True, whis=0.5)
+                                showfliers=False, patch_artist=True, whis=1.5)
             for patch in bplot['boxes']:
                 patch.set_facecolor(mod_colors[this_mod])
             # for patch in bplot['means']:
@@ -244,11 +204,13 @@ for col_ind, roi in enumerate(['start', 'end']):
             plt.xlim([x_zero, len(roi_bin_edges)-1.5])
             plt.xticks(np.arange(x_zero, len(roi_bin_edges)-1), roi_bin_edges[x_zero_ind:])
         plt.axhline(y=0, color='gray', alpha=0.5)
-        plt.ylim([-2.5, 2.5])
+        plt.ylim(ylim)
         # plt.title(f'${{{dict_mod_display[this_mod]}}}$', fontsize=15)
-        plt.legend(*zip(*labels), loc='upper left')
+        plt.legend(*zip(*mod_labels), loc='upper left')
         plt.xlabel(f'Distance from IR {roi}', fontsize=12)
-        plt.ylabel('Mean mod. prob', fontsize=12)
+        plt.ylabel(r'log2fc ($N_{IR}$ / $N_{non-IR}$)', fontsize=12)
+plt.suptitle(f'{ds}, {cond}', fontsize=20)
+plt.savefig(os.path.join(img_out, f'log2gc_by_IR_distance_{ds}_{cond}.png'), bbox_inches='tight')
 
 ########################################################################################################################
 ### output bam file ####################################################################################################
@@ -271,9 +233,21 @@ os.rename(exonic_bamfile.replace('.bam', '.sorted.bam'), exonic_bamfile)
 pysam.index(exonic_bamfile)
 
 ########################################################################################################################
+### pileup #############################################################################################################
+########################################################################################################################
+# mod_sites = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/site_annotations/mus_musculus/GRCm38_102/m6A.psi.GRCm38_102.bed'
+# os.system(
+#     "python3 ~/git/mAFiA_dev/mAFiA/mAFiA_pileup.py"
+#     f" --bam_file {intronic_bamfile}"
+#     f" --mod_file {mod_sites}"
+#     " --min_coverage 1"
+#     f" --out_dir {os.path.join(res_dir, 'IRFinder')}"
+#     f" --out_filename {intronic_bamfile.replace('.bam', '.bed')}"
+# )
+
+########################################################################################################################
 ### avg mod per read ###################################################################################################
 ########################################################################################################################
-
 intronic_mod_level = calc_mod_level_per_read(intronic_reads)
 exonic_mod_level = calc_mod_level_per_read(exonic_reads)
 
@@ -299,4 +273,4 @@ for mod_ind, this_mod in enumerate(mods):
     plt.ylabel('Density', fontsize=12)
     plt.title(f'${{{dict_mod_display[this_mod]}}}$', fontsize=15)
 plt.suptitle(f'{ds}, {cond}', fontsize=20)
-plt.savefig(os.path.join(img_out, f'hist_{ds}_{cond}.png'), bbox_inches='tight')
+# plt.savefig(os.path.join(img_out, f'hist_{ds}_{cond}.png'), bbox_inches='tight')
