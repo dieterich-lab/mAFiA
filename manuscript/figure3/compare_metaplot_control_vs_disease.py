51f22eed0f8c18fcf820bc4dd4d2fd565d1ce71d
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-09-05 13:49:06 +0200

    slightly modified params for mod profile

--- manuscript/figure3/compare_metaplot_control_vs_disease.py
+++ manuscript/figure3/compare_metaplot_control_vs_disease.py
@@ -1,15 +1,13 @@
 import os
 import pandas as pd
 import numpy as np
-import matplotlib
 import matplotlib as mpl
-import matplotlib.pyplot as plt
-
 #######################################################################
 cm = 1/2.54  # centimeters in inches
 gr = 1.618
-# mpl.rcParams['figure.dpi'] = 600
-# mpl.rcParams['savefig.dpi'] = 600
+dpi = 1200
+mpl.rcParams['figure.dpi'] = dpi
+mpl.rcParams['savefig.dpi'] = dpi
 mpl.rcParams['font.size'] = 5
 mpl.rcParams['legend.fontsize'] = 5
 mpl.rcParams['xtick.labelsize'] = 5
@@ -18,12 +16,13 @@ mpl.rcParams['xtick.major.size'] = 1.5
 mpl.rcParams['ytick.major.size'] = 1.5
 mpl.rcParams['lines.linewidth'] = 0.5
 mpl.rcParams['font.family'] = 'Arial'
-FMT = 'pdf'
-fig_kwargs = dict(format=FMT, bbox_inches='tight', dpi=1200)
+FMT = 'svg'
+fig_kwargs = dict(format=FMT, bbox_inches='tight', dpi=dpi, transparent=True)
 #######################################################################
+import matplotlib.pyplot as plt
 
 
-def smooth(y, box_pts=5):
+def smooth(y, box_pts=10):
     box = np.ones(box_pts)/box_pts
     y_smooth = np.convolve(y, box, mode='same')
     return y_smooth
@@ -32,15 +31,11 @@ def smooth(y, box_pts=5):
 # ds = 'HFpEF'
 # conditions = ['ctrl_merged', 'HFpEF_merged']
 
-# ds = 'Diet'
-# conditions = ['WT_CD_merged', 'WT_WD_merged']
+ds = 'Diet'
+conditions = ['WT_CD_merged', 'WT_WD_merged']
 
-ds = 'TAC'
-conditions = ['SHAM_merged', 'TAC_merged']
-
-# conditions = ['below_90', 'above_90']
-# conditions = ['below_50', 'above_150']
-# cond_names = {this_cond: this_cond.replace('_', ' ') + 'nts' for this_cond in conditions}
+# ds = 'TAC'
+# conditions = ['SHAM_merged', 'TAC_merged']
 
 cond_names = {this_cond: this_cond.rstrip('_merged') for this_cond in conditions}
 cond_colors = {this_cond: this_color for this_cond, this_color in zip(conditions, ['blue', 'red'])}
@@ -72,7 +67,7 @@ for this_cond in conditions:
             cond_mod_thresh_dist_measure[this_cond][this_mod][this_thresh] = pd.read_csv(this_dist_measure_file, sep='\t')
 
 hist_range = [0, 3]
-hist_num_bins = 50
+hist_num_bins = 60
 bin_edges = np.linspace(*hist_range, hist_num_bins+1)
 bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
 
@@ -97,9 +92,10 @@ for mod_ind, this_mod in enumerate(mods):
     plt.axvline(x=1, c='gray', alpha=0.5)
     plt.axvline(x=2, c='gray', alpha=0.5)
     plt.xticks([0.5, 1.5, 2.5], ['5\' UTR', 'CDS', '3\' UTR'])
+    # plt.xticks([0.5, 1.5, 2.5], [])
     # plt.ylabel('$N_{S\geq50}$ / $N_{covered}$')
     plt.ylim([0, 0.1])
-    plt.title(rf'${{{dict_mod_display[this_mod]}}}$')
+    # plt.title(rf'${{{dict_mod_display[this_mod]}}}$')
 ymax = np.round((np.max(all_norm_hist) // 0.05 + 1) * 0.05, 2)
 for mod_ind, this_mod in enumerate(mods):
     plt.subplot(1, 2, mod_ind+1)
