1c4d9aa9220454243aa09c63b84bb481b0fcc4eb
Adrian <adrian.kaho.chan@gmail.com>
2024-09-11 13:16:39 +0200

    added chrMT

--- workflows/psico_mAFiA_integrated_mouse.sh
+++ workflows/psico_mAFiA_integrated_mouse.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ds="CM/Cre48"
+ds="CM/IVT"
 
-jid=$(sbatch --parsable --export=ALL,ds="${ds}" --array=1-19,23 "${HOME}"/git/mAFiA_dev/workflows/mAFiA_process_reads_mouse.sh)
+jid=$(sbatch --parsable --export=ALL,ds="${ds}" --array=1-19,23,24 "${HOME}"/git/mAFiA_dev/workflows/mAFiA_process_reads_mouse.sh)
 
-sbatch --dependency=afterok:"${jid}" --export=ALL,ds=${ds} --array=1-19,23 "${HOME}"/git/mAFiA_dev/workflows/mAFiA_pileup_mouse.sh
+sbatch --dependency=afterok:"${jid}" --export=ALL,ds=${ds} --array=1-19,23,24 "${HOME}"/git/mAFiA_dev/workflows/mAFiA_pileup_mouse.sh
