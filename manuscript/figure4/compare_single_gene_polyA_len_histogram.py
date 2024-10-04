e09d4075ccfaa7dba0022d650ee58e044ccc04aa
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-10-04 15:52:29 +0200

    started on polyA tail length

---
+++ manuscript/figure4/compare_single_gene_polyA_len_histogram.py
@@ -0,0 +1,137 @@
+import os
+from glob import glob
+import pandas as pd
+from collections import Counter
+import numpy as np
+from scipy.stats import kstest
+from tqdm import tqdm
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+#######################################################################
+cm = 1/2.54  # centimeters in inches
+gr = 1.618
+# mpl.rcParams['figure.dpi'] = 600
+# mpl.rcParams['savefig.dpi'] = 600
+mpl.rcParams['font.size'] = 5
+mpl.rcParams['legend.fontsize'] = 5
+mpl.rcParams['xtick.labelsize'] = 5
+mpl.rcParams['ytick.labelsize'] = 5
+mpl.rcParams['xtick.major.size'] = 1.5
+mpl.rcParams['ytick.major.size'] = 1.5
+mpl.rcParams['lines.linewidth'] = 0.5
+mpl.rcParams['font.family'] = 'Arial'
+FMT = 'svg'
+fig_kwargs = dict(format=FMT, bbox_inches='tight', dpi=1200, transparent=True)
+#######################################################################
+
+
+def get_df_log2fc(in_cond_df):
+    common_genes = list(set(in_cond_df['SHAM']['gene'].unique()).intersection(set(in_cond_df['TAC']['gene'].unique())))
+    out_gene_polyA_log2fc_pval = {}
+    for thisGene in tqdm(common_genes):
+        polyA_len_0 = in_cond_df[conditions[0]][in_cond_df[conditions[0]]['gene'] == thisGene]['polyA_length']
+        polyA_len_1 = in_cond_df[conditions[1]][in_cond_df[conditions[1]]['gene'] == thisGene]['polyA_length']
+        mean_len_0 = np.mean(polyA_len_0)
+        mean_len_1 = np.mean(polyA_len_1)
+        log2fc = np.log2(mean_len_1 / mean_len_0)
+        ks_stat, pval = kstest(polyA_len_0, polyA_len_1)
+        out_gene_polyA_log2fc_pval[thisGene] = (len(polyA_len_0), len(polyA_len_1), mean_len_0, mean_len_1, log2fc, pval)
+
+    out_df_gene_polyA_log2fc_pval = pd.DataFrame([[k] + list(v) for k, v in out_gene_polyA_log2fc_pval.items()],
+                                             columns=['gene',
+                                                      f'num_reads_{conditions[0]}',
+                                                      f'num_reads_{conditions[1]}',
+                                                      f'mean_polyA_len_{conditions[0]}',
+                                                      f'mean_polyA_len_{conditions[1]}',
+                                                      'log2fc',
+                                                      'pval'
+                                                      ])
+    out_df_gene_polyA_log2fc_pval.sort_values('log2fc', inplace=True)
+    return out_df_gene_polyA_log2fc_pval
+
+
+polyA_dir = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/polyA'
+img_out = '/home/adrian/img_out/manuscript_psico_mAFiA/figure4'
+
+ds = 'TAC'
+conditions = ['SHAM', 'TAC']
+cond_colors = {this_cond: this_color for this_cond, this_color in zip(conditions, ['b', 'r'])}
+
+log2fc_filename = os.path.join(polyA_dir, f'gene_polyA_log2fc_pval_{ds}.tsv')
+
+cond_df = {}
+for this_cond in conditions:
+    all_filepaths = glob(os.path.join(polyA_dir, f'polyA_reads_annotated_{ds}_{this_cond}_day*.tsv'))
+    all_dfs = []
+    for this_filepath in all_filepaths:
+        all_dfs.append(pd.read_csv(this_filepath, sep='\t'))
+    cond_df[this_cond] = pd.concat(all_dfs).sort_values('gene')
+
+if os.path.exists(log2fc_filename):
+    df_gene_polyA_log2fc_pval = pd.read_csv(log2fc_filename, sep='\t')
+else:
+    df_gene_polyA_log2fc_pval = get_df_log2fc(cond_df)
+    df_gene_polyA_log2fc_pval.to_csv(log2fc_filename, sep='\t', index=False, float_format='%.6f')
+
+
+### volcano plot ###
+df_gene_polyA_log2fc_pval['logit'] = -np.log10(df_gene_polyA_log2fc_pval['pval'])
+
+thresh_logit = 3
+thresh_log2fc = 0.0
+
+positive_mask = (df_gene_polyA_log2fc_pval['log2fc'].values >= thresh_log2fc) \
+                * (df_gene_polyA_log2fc_pval['logit'].values >= thresh_logit)
+negative_mask = (df_gene_polyA_log2fc_pval['log2fc'].values < -thresh_log2fc) \
+                * (df_gene_polyA_log2fc_pval['logit'].values >= thresh_logit)
+positive_genes = df_gene_polyA_log2fc_pval['gene'].values[positive_mask]
+negative_genes = df_gene_polyA_log2fc_pval['gene'].values[negative_mask]
+
+xmax = 2
+ymax = 5.5
+xticks = np.linspace(-xmax, xmax, 5)
+yticks = np.arange(0, ymax+0.01, 1)
+
+plt.figure(figsize=(4*cm, 4*cm))
+plt.scatter(df_gene_polyA_log2fc_pval['log2fc'], df_gene_polyA_log2fc_pval['logit'], c='gray', s=1, alpha=0.5)
+plt.scatter(df_gene_polyA_log2fc_pval['log2fc'].values[positive_mask],
+            df_gene_polyA_log2fc_pval['logit'].values[positive_mask], c='red', s=1)
+plt.scatter(df_gene_polyA_log2fc_pval['log2fc'].values[negative_mask],
+            df_gene_polyA_log2fc_pval['logit'].values[negative_mask], c='blue', s=1)
+plt.xlim([-2, 2])
+plt.ylim([0, 5.5])
+plt.xticks(xticks)
+plt.yticks(yticks)
+plt.text(-xmax+0.05, ymax-0.05, '\n'.join(negative_genes), c='b', ha='left', va='top')
+plt.text(xmax-0.05, ymax-0.05, '\n'.join(positive_genes), c='r', ha='right', va='top')
+
+plt.savefig(os.path.join(img_out, f'volcano_plot_polyA_len_log2fc_pval_{ds}.{FMT}'), **fig_kwargs)
+
+
+### single transcript distribution ###
+this_gene = 'Mb'
+gene_cond_distribution = {}
+for this_cond in conditions:
+    this_cond_df = cond_df[this_cond]
+    gene_cond_distribution[this_cond] = this_cond_df[this_cond_df['gene'] == this_gene]['polyA_length']
+
+xmax = 300
+ymax = 0.17
+xticks = np.linspace(0, xmax, 4)
+yticks = np.arange(0, ymax+0.01, 0.1)
+bin_width = 10
+num_bins = np.int64(xmax / bin_width)
+bin_edges = np.linspace(0, xmax, num_bins+1)
+bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+
+plt.figure(figsize=(2*cm, 2*cm))
+for this_cond in conditions:
+    this_hist, _ = np.histogram(gene_cond_distribution[this_cond], bins=bin_edges)
+    norm_hist = this_hist / np.sum(this_hist)
+    plt.plot(bin_centers, norm_hist, c=cond_colors[this_cond], label=this_cond)
+plt.xticks(xticks)
+plt.yticks(yticks)
+plt.xlim([0, xmax])
+plt.ylim([0, ymax])
+plt.savefig(os.path.join(img_out, f'hist_polyA_len_{ds}_{this_gene}.{FMT}'), **fig_kwargs)
--- manuscript/figure4/plot_polyA_distribution.py
+++ manuscript/figure4/plot_polyA_distribution.py
@@ -19,52 +19,10 @@ mpl.rcParams['xtick.major.size'] = 1.5
 mpl.rcParams['ytick.major.size'] = 1.5
 mpl.rcParams['lines.linewidth'] = 0.5
 mpl.rcParams['font.family'] = 'Arial'
-FMT = 'pdf'
-fig_kwargs = dict(format=FMT, bbox_inches='tight', dpi=1200)
+FMT = 'svg'
+fig_kwargs = dict(format=FMT, bbox_inches='tight', dpi=1200, transparent=True)
 #######################################################################
 import matplotlib.pyplot as plt
-
-
-def calc_mod_level_per_read(in_bam_file, num_bases=5):
-    read_avg_mod_level = {}
-    with pysam.AlignmentFile(in_bam_file, 'rb') as in_bam:
-        for this_read in tqdm(in_bam.fetch()):
-            read_avg_mod_level[this_read.query_name] = {}
-            for this_mod in dict_mod_code.keys():
-                mod_bases = this_read.modified_bases.get(('N', 0, dict_mod_code[this_mod]), [])
-
-                if len(mod_bases)>=num_bases:
-                    read_avg_mod_level[this_read.query_name][this_mod] = np.mean(np.partition([tup[1] for tup in mod_bases], -num_bases)[-num_bases:]) / 255.0
-                else:
-                    read_avg_mod_level[this_read.query_name][this_mod] = np.mean([tup[1] for tup in mod_bases]) / 255.0
-
-    return read_avg_mod_level
-
-
-def get_read_norm_pos(in_bam_file, thresh_read_ids=[], num_bases=5):
-    def main_function():
-        mod_bases = this_read.modified_bases.get(('N', 0, dict_mod_code[this_mod]), [])
-        if len(mod_bases):
-            vec_pos, vec_mod_prob = np.vstack(mod_bases).T
-            if len(mod_bases) > num_bases:
-                sel_pos = vec_pos[np.argpartition([tup[1] for tup in mod_bases], -num_bases)[-num_bases:]]
-            else:
-                sel_pos = vec_pos
-            if this_read.flag == 0:
-                norm_pos = sel_pos / this_read.query_length
-            elif this_read.flag == 16:
-                norm_pos = (this_read.query_length - sel_pos - 1) / this_read.query_length
-            read_norm_pos[this_read.query_name] = norm_pos
-
-    read_norm_pos = {}
-    with pysam.AlignmentFile(in_bam_file, 'rb') as in_bam:
-        for this_read in tqdm(in_bam.fetch()):
-            if len(thresh_read_ids):
-                if this_read.query_name in thresh_read_ids:
-                    main_function()
-            else:
-                main_function()
-    return read_norm_pos
 
 
 mods = ['m6A', 'psi']
@@ -81,11 +39,11 @@ dict_mod_code = {
 ########################################################################################################################
 ### define datasets ####################################################################################################
 ########################################################################################################################
-# ds = 'TAC'
-# conditions = ['SHAM', 'TAC']
+ds = 'TAC'
+conditions = ['SHAM', 'TAC']
 
-ds = 'HFpEF'
-conditions = ['ctrl', 'HFpEF']
+# ds = 'HFpEF'
+# conditions = ['ctrl', 'HFpEF']
 
 # ds = 'Diet'
 # conditions = ['WT_CD', 'WT_WD']
@@ -94,9 +52,6 @@ cond_colors = {this_cond: this_color for this_cond, this_color in zip(conditions
 
 polyA_dir = f'/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/{ds}/polyA'
 polyA_files = {this_cond: os.path.join(polyA_dir, f'{this_cond}_read2polyA_length.txt') for this_cond in conditions}
-
-bam_dir = f'/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/{ds}'
-bam_files = {this_cond: os.path.join(bam_dir, f'{this_cond}_merged/chrALL.mAFiA.reads.bam') for this_cond in conditions}
 
 img_out = '/home/adrian/img_out/manuscript_psico_mAFiA/figure4'
 os.makedirs(img_out, exist_ok=True)
@@ -109,110 +64,55 @@ df_polyA = {this_cond: pd.read_csv(polyA_files[this_cond], sep='\t', names=['rea
 dict_polyA = {this_cond: {k: v for k, v in df_polyA[this_cond][['read_id', 'polyA_len']].values}
               for this_cond in conditions}
 
-num_bins = 100
-bin_edges = np.linspace(0, 500, num_bins+1)
+xmax = 400
+ymax = 0.25
+xticks = np.linspace(0, xmax, 5)
+yticks = np.arange(0, ymax+0.01, 0.1)
+
+bin_width = 10
+num_bins = np.int64(xmax / bin_width)
+bin_edges = np.linspace(0, xmax, num_bins+1)
 bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
 
-plt.figure(figsize=(8*cm, 3*cm))
-plt.subplot(1, 2, 1)
-for cond_ind, this_cond in enumerate(conditions):
+### linear only ###
+plt.figure(figsize=(4*cm, 4*cm))
+# this_cond = conditions[0]
+for this_cond in conditions:
     # plt.hist(dict_polyA[this_cond].values(), range=[0, 500], bins=50, label=this_cond, alpha=0.5, density=True)
     this_hist, _ = np.histogram(list(dict_polyA[this_cond].values()), bins=bin_edges)
     norm_hist = this_hist / np.sum(this_hist)
     plt.plot(bin_centers, norm_hist, c=cond_colors[this_cond], label=this_cond)
-# plt.legend(fontsize=10)
+plt.legend(loc='lower right')
 # plt.xlabel('polyA length (bps)', fontsize=12)
 # plt.ylabel('Density', fontsize=12)
-plt.subplot(1, 2, 2)
-for cond_ind, this_cond in enumerate(conditions):
-    # plt.hist(dict_polyA[this_cond].values(), range=[0, 500], bins=50, label=this_cond, alpha=0.5, density=True)
-    this_hist, _ = np.histogram(list(dict_polyA[this_cond].values()), bins=bin_edges)
-    norm_hist = this_hist / np.sum(this_hist)
-    plt.plot(bin_centers, norm_hist, c=cond_colors[this_cond], label=this_cond)
-    plt.yscale('log')
-plt.minorticks_off()
-plt.yticks([10**-5, 10**-3, 10**-1])
-# plt.legend(fontsize=10)
-# plt.xlabel('polyA length (bps)', fontsize=12)
-# plt.ylabel('Density (log)', fontsize=12)
-# plt.suptitle(ds, fontsize=15)
-plt.savefig(os.path.join(img_out, f'hist_polyA_len_{ds}_{conditions[0]}_vs_{conditions[1]}.{FMT}'), **fig_kwargs)
+plt.xticks(xticks)
+plt.yticks(yticks)
+plt.xlim([0, xmax])
+plt.ylim([0, ymax])
+plt.savefig(os.path.join(img_out, f'hist_polyA_len_{ds}.{FMT}'), **fig_kwargs)
 
-########################################################################################################################
-### subset BAM files ###################################################################################################
-########################################################################################################################
-upper_lim = 90
-lower_lim = 90
-
-polyA_thresholds = [f'below_{lower_lim}', f'above_{upper_lim}']
-
-# for this_cond, this_df in df_polyA.items():
-#     above_read_ids = this_df[this_df['polyA_len'] >= upper_lim]['read_id'].tolist()
-#     above_read_ids_file = os.path.join(polyA_dir, f'{this_cond}_read_ids_polyA_len_above_{upper_lim}.txt')
-#     above_bam_file = os.path.join(polyA_dir, f'{this_cond}_polyA_len_above_{upper_lim}.bam')
-#
-#     with open(above_read_ids_file, 'w') as f_out:
-#         for this_read_id in above_read_ids:
-#             f_out.writelines(this_read_id + '\n')
-#     pysam.view("-N", above_read_ids_file, "-o", above_bam_file, bam_files[this_cond], catch_stdout=False)
-#     pysam.index(above_bam_file)
-#
-#     below_read_ids = this_df[this_df['polyA_len'] < lower_lim]['read_id'].tolist()
-#     below_read_ids_file = os.path.join(polyA_dir, f'{this_cond}_read_ids_polyA_len_below_{lower_lim}.txt')
-#     below_bam_file = os.path.join(polyA_dir, f'{this_cond}_polyA_len_below_{lower_lim}.bam')
-#     with open(below_read_ids_file, 'w') as f_out:
-#         for this_read_id in below_read_ids:
-#             f_out.writelines(this_read_id + '\n')
-#     pysam.view("-N", below_read_ids_file, "-o", below_bam_file, bam_files[this_cond], catch_stdout=False)
-#     pysam.index(below_bam_file)
-
-########################################################################################################################
-### top mod. levels per read ###########################################################################################
-########################################################################################################################
-# mod_level_per_read = {
-#     this_cond: {
-#         polyA_suffix: calc_mod_level_per_read(os.path.join(polyA_dir, f'{this_cond}_polyA_len_{polyA_suffix}.bam'))
-#         for polyA_suffix in polyA_thresholds
-#     }
-#     for this_cond in conditions
-# }
-#
-# num_bins = 20
-# max_bin = 1.0
-#
-# cond_ls = {
-#     this_cond: this_ls for this_cond, this_ls in zip(conditions, [':', '-'])
-# }
-#
-# polyA_text = [
-#     f'$< {lower_lim}$',
-#     # '[50, 150)',
-#     f'$\geq {upper_lim}$'
-# ]
-# polyA_colors = [
-#     'skyblue',
-#     # 'royalblue',
-#     'midnightblue'
-# ]
-#
-# plt.figure(figsize=(6*cm, 3*cm))
-# for mod_ind, this_mod in enumerate(mods):
-#     plt.subplot(1, 2, mod_ind+1)
-#     for this_polyA_thresh, this_polyA_text, this_polyA_color in zip(polyA_thresholds, polyA_text, polyA_colors):
-#         for this_cond in conditions:
-#             this_mod_level_per_read = mod_level_per_read[this_cond][this_polyA_thresh]
-#             this_mod_vals = [val.get(this_mod, np.nan) for val in this_mod_level_per_read.values()]
-#             this_hist, bin_edges = np.histogram(this_mod_vals, range=[0, max_bin], bins=num_bins)
-#             this_hist = this_hist / np.sum(this_hist)
-#             bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2
-#             plt.plot(bin_centers, this_hist, c=this_polyA_color,
-#                      ls=cond_ls[this_cond], label=f'{this_cond}, {this_polyA_text}')
-#     plt.xlim([-0.01, 1.01])
-#     plt.ylim([0, 0.1])
-#     # plt.legend(title='poly(A) len, bps')
-#     # plt.xlabel(rf'$\langle P({{{dict_mod_display[this_mod]}}}) \rangle $ per read', fontsize=12)
-#     # plt.ylabel('Density', fontsize=12)
-#     # plt.title(rf'${{{dict_mod_display[this_mod]}}}$', fontsize=15)
-# # plt.suptitle(f'{conditions[0]} vs {conditions[1]}', fontsize=15)
-# plt.savefig(os.path.join(img_out, f'mod_per_read_{ds}_{conditions[0]}_vs_{conditions[1]}.{FMT}'), **fig_kwargs)
-
+### linear and log ###
+# plt.figure(figsize=(8*cm, 3*cm))
+# plt.subplot(1, 2, 1)
+# for cond_ind, this_cond in enumerate(conditions):
+#     # plt.hist(dict_polyA[this_cond].values(), range=[0, 500], bins=50, label=this_cond, alpha=0.5, density=True)
+#     this_hist, _ = np.histogram(list(dict_polyA[this_cond].values()), bins=bin_edges)
+#     norm_hist = this_hist / np.sum(this_hist)
+#     plt.plot(bin_centers, norm_hist, c=cond_colors[this_cond], label=this_cond)
+# # plt.legend(fontsize=10)
+# # plt.xlabel('polyA length (bps)', fontsize=12)
+# # plt.ylabel('Density', fontsize=12)
+# plt.subplot(1, 2, 2)
+# for cond_ind, this_cond in enumerate(conditions):
+#     # plt.hist(dict_polyA[this_cond].values(), range=[0, 500], bins=50, label=this_cond, alpha=0.5, density=True)
+#     this_hist, _ = np.histogram(list(dict_polyA[this_cond].values()), bins=bin_edges)
+#     norm_hist = this_hist / np.sum(this_hist)
+#     plt.plot(bin_centers, norm_hist, c=cond_colors[this_cond], label=this_cond)
+#     plt.yscale('log')
+# plt.minorticks_off()
+# plt.yticks([10**-5, 10**-3, 10**-1])
+# # plt.legend(fontsize=10)
+# # plt.xlabel('polyA length (bps)', fontsize=12)
+# # plt.ylabel('Density (log)', fontsize=12)
+# # plt.suptitle(ds, fontsize=15)
+# plt.savefig(os.path.join(img_out, f'hist_polyA_len_{ds}_{conditions[0]}_vs_{conditions[1]}.{FMT}'), **fig_kwargs)
