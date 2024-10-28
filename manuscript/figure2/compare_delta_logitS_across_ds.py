2248a969bc0ccd47249e1433fda6f79b5b9379d7
Adrian Chan <adrian.kaho.chan@gmail.com>
2024-10-28 11:24:55 +0100

    added contour plot to figure 2

--- manuscript/figure2/compare_delta_logitS_across_ds.py
+++ manuscript/figure2/compare_delta_logitS_across_ds.py
@@ -22,6 +22,8 @@ FMT = 'svg'
 fig_kwargs = dict(format=FMT, bbox_inches='tight', dpi=dpi, transparent=True)
 #######################################################################
 import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+import scipy
 
 in_dir = '/home/adrian/Data/TRR319_RMaP_BaseCalling/Adrian/results/psico-mAFiA_v1/mouse_heart/transcript_logit'
 img_out = '/home/adrian/img_out/manuscript_psico_mAFiA/figure2'
@@ -32,6 +34,11 @@ ds_display = {
     'HFpEF': 'HFpEF /\nCtrl',
     'Diet': 'WD /\nCD'
 }
+ds_color = {
+    'TAC': 'g',
+    'HFpEF': 'r',
+    'Diet': 'b'
+}
 mods = ['m6A', 'psi']
 dict_mod_display = {
     'm6A': 'm^6A',
@@ -39,7 +46,7 @@ dict_mod_display = {
 }
 
 thresh_pval = 0.05
-num_gene_mods = 20
+num_gene_mods = 50
 
 dfs = {}
 for this_ds in ds:
@@ -80,7 +87,7 @@ sorted_mat_delta_logit = sel_mat_delta_logit[sorted_indices]
 sorted_gene_mod = np.array(sel_gene_mod)[sorted_indices]
 
 vmax = 0.5
-plt.figure(figsize=(4*cm, 8.5*cm))
+plt.figure(figsize=(4*cm, 15*cm))
 plt.imshow(sorted_mat_delta_logit, aspect='auto', origin='lower', vmin=-vmax, vmax=vmax, cmap='seismic')
 plt.xticks(np.arange(len(ds)), list(ds_display.values()))
 plt.yticks(np.arange(num_gene_mods), sorted_gene_mod)
@@ -88,3 +95,53 @@ plt.gca().xaxis.tick_top()
 cbar = plt.colorbar()
 cbar.set_ticks(np.linspace(-vmax, vmax, 5))
 plt.savefig(os.path.join(img_out, f'heatmap_delta_logit_S_all_ds.{FMT}'), **fig_kwargs)
+
+### scatter plots in (m6A, psi) space ###
+ds_vec_mods = {this_ds: [] for this_ds in ds}
+for this_ds in ds:
+    this_df = dfs[this_ds]
+    unique_genes = this_df['gene'].unique()
+    for this_gene in unique_genes:
+        sub_df = this_df[
+            (this_df['gene'] == this_gene)
+            # * (this_df[f'pval_{this_ds}'] < 0.001)
+            ]
+        if (len(sub_df['mod']) == 2) and set(mods).issubset(set(sub_df['mod'].values)):
+            sub_df.sort_values('mod', inplace=True)
+            ds_vec_mods[this_ds].append(sub_df[f'delta_logit_{this_ds}'].values)
+
+ds_cmap = {
+    'TAC': 'Greens',
+    'HFpEF': 'Reds',
+    'Diet': 'Blues'
+}
+
+xmax = 0.7
+zoom_factor = 2
+num_bins = 20
+levels = 3
+ticks = np.linspace(-xmax, xmax, 3)
+plt.figure(figsize=(13*cm, 4*cm))
+for ds_ind, this_ds in enumerate(ds):
+    plt.subplot(1, 3, ds_ind+1)
+    plt.axvline(x=0, c='gray', ls='--', alpha=0.5)
+    plt.axhline(y=0, c='gray', ls='--', alpha=0.5)
+    vec_x, vec_y = np.vstack(ds_vec_mods[this_ds]).T
+    # plt.scatter(vec_x, vec_y, s=1, c=ds_color[this_ds], label=ds_display[this_ds])
+    mat_z, bin_y, bin_x = np.histogram2d(vec_x, vec_y, range=[[-xmax, xmax], [-xmax, xmax]], bins=num_bins)
+    mat_z = scipy.ndimage.zoom(mat_z, zoom_factor)
+    mat_z[mat_z < 10] = 0
+    # center_x = 0.5 * (bin_x[1:] + bin_x[:-1])
+    zoom_bin_x = np.linspace(-xmax, xmax, num_bins*zoom_factor+1)
+    zoom_center_x = 0.5 * (zoom_bin_x[1:] + zoom_bin_x[:-1])
+    mat_x, mat_y = np.meshgrid(zoom_center_x, zoom_center_x)
+    plt.contourf(mat_x, mat_y, mat_z, levels=levels, cmap=ds_cmap[this_ds])
+    # plt.contourf(mat_z, levels=3, cmap=ds_cmap[this_ds], vmin=20, alpha=0.5)
+    plt.xticks(ticks)
+    if ds_ind == 0:
+        plt.yticks(ticks)
+    else:
+        plt.yticks(ticks, [])
+    plt.xlim([-xmax, xmax])
+    plt.ylim([-xmax, xmax])
+plt.savefig(os.path.join(img_out, f'contour_delta_logit_S_all_ds.{FMT}'), **fig_kwargs)
